b5a3d9d76e0c6c57b1b7547744f1044b184756f3
S Thomson <spth@roe.ac.uk>
2013-08-15 19:36:29 +0100

    Builder can take into account n_per_leaf > 1. Tree is NOT compacted, though.

--- karras-tree/builder.py
+++ karras-tree/builder.py
@@ -42,10 +42,11 @@ class Node(object):
         return False
 
 class LeafNode(object):
-    def __init__(self, index, parent_node):
+    def __init__(self, index, parent_node, span=1):
         super(LeafNode, self).__init__()
         self.index = index
         self._parent = parent_node
+        self._span = span
 
     @classmethod
     def empty(cls):
@@ -59,12 +60,16 @@ class LeafNode(object):
     def parent(self, parent_node):
         self._parent = parent_node
 
+    @property
+    def span(self):
+        return self._span
+
     def is_leaf(self):
         return True
 
 class BinRadixTree(object):
     """Represents a binary radix tree.  Contains methods for tree construction."""
-    def __init__(self, primitives=[]):
+    def __init__(self, primitives=[], n_per_leaf=1):
         """
         Initialize BinRadixTree from a list of (x,y,z) primitive co-ordinates.
 
@@ -74,11 +79,14 @@ class BinRadixTree(object):
         super(BinRadixTree, self).__init__()
         self.primitives = primitives
         self.n_primitives = len(primitives)
+        self.n_per_leaf = n_per_leaf
+        self.n_nodes = self.n_primitives - self.n_per_leaf
+        self.n_leaves = self.n_nodes + 1
 
         self.keys = self.generate_keys()
 
-        self.nodes = [Node.empty() for i in range(self.n_primitives-1)]
-        self.leaves = [LeafNode.empty() for i in range(self.n_primitives)]
+        self.nodes = [Node.empty() for i in range(self.n_nodes)]
+        self.leaves = [LeafNode.empty() for i in range(self.n_leaves)]
 
     @classmethod
     def from_primitives(cls, primitives):
@@ -91,19 +99,16 @@ class BinRadixTree(object):
         tree.build()
         return tree
 
-    def _common_prefix(self, i, j):
-        "Finds the longest common prefix of keys at indices i, j."
-        if j < 0 or j > self.n_primitives-1:
-            return -1
-
-        key_i, key_j = self.keys[i], self.keys[j]
-
-        prefix_length = bits.common_prefix(key_i, key_j)
-        if prefix_length == 32:
-            # Identical keys.  Increase length of prefix using key indices.
-            print "Identical keys!"
-            prefix_length += bits.common_prefix(i, j)
-        return prefix_length
+    @classmethod
+    def short_from_primitives(cls, primitives, n_per_leaf):
+        "Construct a short BinRadixTree from (x,y,z) positions and a max leaf size. "
+        print("Allocating arrays, generating keys...")
+        tree = cls(primitives, n_per_leaf)
+        print("Sorting keys...")
+        tree.sort_primitives_by_keys()
+        print("Building short tree..")
+        tree.build_short()
+        return tree
 
     def generate_keys(self):
         "Returns a list of morton keys, one for each primitive."
@@ -125,76 +130,124 @@ class BinRadixTree(object):
         self.keys, self.primitives = [list(t) for t in zip(*packed_tuple)]
 
     def build(self):
-        "Builds a binary radix tree based on the list of primitives and their keys."
-        if len(self.nodes) != self.n_primitives-1:
-            self.nodes = [Node.empty() for i in range(self.n_primitives-1)]
-        if len(self.leaves) != self.n_primitives:
-            self.leaves = [LeafNode(None) for i in range(self.n_primitives)]
+        "Builds a binary radix tree from the list of primitives and their keys."
         # This loop can be done in parallel.
-        for node_idx in range(self.n_primitives-1):
-            i = node_idx
+        for node_index in range(self.n_nodes):
             # Get the 'direction' of the node.
             # d = +1 => node begins at i (right branch).
             # d = -1 => node ends at i (left branch).
-            d = np.sign(self._common_prefix(i, i+1) - self._common_prefix(i, i-1))
+            direction = self._node_direction(node_index)
 
-            # The lower bound for this node's prefix length comes from the
-            # difference between its key and the key of its sibling.  (All keys
-            # within this node must have a longer common prefix than this.)
-            min_common_prefix = self._common_prefix(i, i-d)
+            end_index = self._find_node_end(node_index, direction)
 
-            # Find an upper bound on this node's range of keys.
-            l_max = 2
-            while self._common_prefix(i, i + l_max*d) > min_common_prefix:
-                # Since nodes are grouped into pairs, we don't have to search
-                # through each value of l_max, only one for each possible decendant.
-                l_max *= 2
-
-            # Perform a binary search in [0, l_max-1] for the range end, j,
-            # making use of the fact that everything in this node has a common
-            # prefix longer than the prefix common to this node and its sibling.
-            l = 0
-            t = l_max / 2
-            while t >= 1:
-                if self._common_prefix(i, i + (l+t)*d) > min_common_prefix:
-                    l += t
-                t /= 2
-            j = i + l*d
-
-            # Perform a binary seach in [i,j] for the split position,
-            # making use of the fact that everything before the split has a
-            # longer common prefix than the node's prefix.
-            node_prefix = self._common_prefix(i, j)
-            s = 0
-            t = l
-            while True:
-                # t = ceil(l/2), ceil(l/4), ...
-                t = (t+1) / 2
-                if self._common_prefix(i, i + (s+t)*d) > node_prefix:
-                    s += t
-                if t == 1:
-                    break
-            split_idx = i + s*d + min(d,0)
-
-            # Output child nodes/leaves, and set parents/children where possible.
-            # Leaves are processed only once, so we can explicity construct them
-            # here.
-            # Nodes are processed twice (first by their parent, then by
-            # themselves), so we may only update their properties.
-            this_node = self.nodes[i]
-            this_node.index = i
-            if min(i,j) == split_idx:
-                #print "Leaf at", split_idx
-                left = self.leaves[split_idx] = LeafNode(split_idx, this_node)
+            split_index = self._find_split_index(node_index,
+                                                 end_index,
+                                                 direction)
+            #self._update_node(node_index, end_index, split_index)
+            # For self.n_per_leaf, this is always true.  (A node spans >= 2 keys.)
+            if abs(end_index - node_index) + 1 > self.n_per_leaf:
+                self._update_node(node_index, end_index, split_index)
             else:
-                left = self.nodes[split_idx]
-                left.parent = this_node
-            if max(i,j) == split_idx + 1:
-                #print "Leaf at", split_idx + 1
-                right = self.leaves[split_idx+1] = LeafNode(split_idx+1,
-                                                            this_node)
-            else:
-                right = self.nodes[split_idx+1]
-                right.parent = this_node
-            this_node.left = left
-            this_node.right = right
+                print abs(end_index - node_index)
+                # This node will be processed as a leaf by its parent,
+                # or is the would-be child of a would-be node.
+                self._make_null_node(node_index)
+
+        if self.n_per_leaf > 1:
+            self._compact_tree()
+
+    def _compact_tree(self):
+        pass
+
+    def _node_direction(self, i):
+        return np.sign(self._common_prefix(i, i+1) -
+                       self._common_prefix(i, i-1))
+
+    def _common_prefix(self, i, j):
+        "Returns the longest common prefix of keys at indices i, j."
+        if j < 0 or j > self.n_primitives-1:
+            return -1
+
+        key_i, key_j = self.keys[i], self.keys[j]
+
+        prefix_length = bits.common_prefix(key_i, key_j)
+        if prefix_length == 32:
+            # Identical keys.  Increase length of prefix using key indices.
+            print "Identical keys!"
+            prefix_length += bits.common_prefix(i, j)
+        return prefix_length
+
+    def _find_node_end(self, i, d):
+        "Returns the other end of the node starting at i, with direction d."
+        # The lower bound for this node's prefix length comes from the
+        # difference between its key and the key of its sibling.  (All keys
+        # within this node must have a longer common prefix than this.)
+        min_common_prefix = self._common_prefix(i, i-d)
+
+        # Find an upper bound on this node's range of keys.
+        l_max = 2
+        while self._common_prefix(i, i + l_max*d) > min_common_prefix:
+            # Since nodes are grouped into pairs, we don't have to search
+            # through each value of l_max, only one for each possible decendant.
+            l_max *= 2
+
+        # Perform a binary search in [0, l_max-1] for the range end, j,
+        # making use of the fact that everything in this node has a common
+        # prefix longer than the prefix common to this node and its sibling.
+        l = 0
+        t = l_max / 2
+        while t >= 1:
+            if self._common_prefix(i, i + (l+t)*d) > min_common_prefix:
+                l += t
+            t /= 2
+        return i + l*d
+
+    def _find_split_index(self, i, j, d):
+        "Returns the split index for node starting at i and ending at j, with direction d."
+        # Perform a binary seach in [i,j] for the split position,
+        # making use of the fact that everything before the split has a
+        # longer common prefix than the node's prefix.
+        node_prefix = self._common_prefix(i, j)
+        s = 0
+        t = (j - i) * d
+        while True:
+            # t = ceil(l/2), ceil(l/4), ... (in case l is odd).
+            t = (t+1) / 2
+            if self._common_prefix(i, i + (s+t)*d) > node_prefix:
+                s += t
+            if t == 1:
+                break
+        # If d = -1, then we actually found split_idx + 1.
+        return i + s*d + min(d,0)
+
+    def _update_node(self, i, j, split_idx):
+        # Output child nodes/leaves, and set parents/children where possible.
+        # Leaves are processed only once, so we can explicity construct them
+        # here.
+        # Nodes are processed twice (first by their parent, then by
+        # themselves), so we may only update their properties.
+        this_node = self.nodes[i]
+        this_node.index = i
+        left_child_span = split_idx - min(i,j) + 1
+        right_child_span = max(i,j) - split_idx
+
+        if left_child_span <= self.n_per_leaf:
+            left = self.leaves[split_idx] = LeafNode(split_idx, this_node,
+                                                     left_child_span)
+        else:
+            left = self.nodes[split_idx]
+            left.parent = this_node
+
+        if right_child_span <= self.n_per_leaf:
+            right = self.leaves[split_idx+1] = LeafNode(split_idx+1, this_node,
+                                                        right_child_span)
+        else:
+            right = self.nodes[split_idx+1]
+            right.parent = this_node
+
+        this_node.left = left
+        this_node.right = right
+
+    def _make_null_node(self, i):
+        self.nodes[i] = Node.empty()
+
