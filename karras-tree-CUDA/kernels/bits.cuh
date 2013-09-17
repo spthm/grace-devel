0839597b42b42627acdb96b61b2ad4664c123497
S Thomson <spth@roe.ac.uk>
2013-09-17 18:46:46 +0100

    Added #pragma once to all headers. Need to check status of nvcc linker.  Can the C-like functions which call the kernels be in separate files? (See bintree_build.cu/h and kernels/bintree_build_kernels.)

--- karras-tree-CUDA/kernels/bits.cuh
+++ karras-tree-CUDA/kernels/bits.cuh
@@ -1,3 +1,5 @@
+#pragma once
+
 namespace grace {
 
 namespace gpu {
--- karras-tree-CUDA/types.h
+++ karras-tree-CUDA/types.h
@@ -1,6 +1,6 @@
-#include <stdint.h>
-
 #pragma once
+
+#include <stdint.h>
 
 #define UInteger32 uint32_t
 #define UInteger32 uint64_t
