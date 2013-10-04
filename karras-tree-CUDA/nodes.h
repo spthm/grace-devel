0772e21001c71c591e5e7c7717c7e44c78ed29c1
S Thomson <sp.thomson@hotmail.com>
2013-10-04 17:41:39 +0100

    Typo in comment.

--- karras-tree-CUDA/nodes.h
+++ karras-tree-CUDA/nodes.h
@@ -9,7 +9,7 @@ struct Node
     // A 32-bit int allows for up to 2,147,483,647 > 1024^3.
     // Since we likely can't fit more than 1024^3 particles on the GPU
     // anyway, int32 should be sufficient for the indices.
-    // Could use UInteger32 and set the root node to 1.  Then common_prefix
+    // Could use uint32 and set the root node to 1.  Then common_prefix
     // may return 0 for out-of-range results, rather than -1.
     Integer32 left;
     Integer32 right;
