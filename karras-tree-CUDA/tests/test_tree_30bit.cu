3fda4c33f4e1eb065fd4bdba1e7e80fcaf5bc441
S Thomson <sp.thomson@hotmail.com>
2013-10-11 01:24:30 +0100

    Added Python code to verify CUDA-generated tree. Updated CUDA tree tester to SORT THE CODES first!

--- karras-tree-CUDA/tests/test_tree_30bit.cu
+++ karras-tree-CUDA/tests/test_tree_30bit.cu
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <iomanip>
 #include <bitset>
+#include <cstdlib>
 
 #include <thrust/random.h>
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
+#include <thrust/sort.h>
 
 #include "../types.h"
 #include "../nodes.h"
@@ -70,7 +72,8 @@ int main(int argc, char* argv[]) {
 
     /* Generate N random position vectors, i.e. 3*N random floats in [0,1) */
 
-    unsigned int N = 10000;
+    unsigned int N = (unsigned int) std::strtol(argv[1], NULL, 10);
+    std::cout << "Will generate " << N << " random points." << std::endl;
     thrust::device_vector<Vector3f> d_centres(N);
 
     thrust::transform(thrust::counting_iterator<unsigned int>(0),
@@ -101,17 +104,24 @@ int main(int argc, char* argv[]) {
                       grace::morton_key_functor<UInteger32, float>(bottom, top) );
 
 
+    /* Sort the position vectors by their keys. Note that this leaves the
+     * radii UN-sorted, but for the purposes of this we don't care.
+     */
+
+    thrust::sort_by_key(d_keys.begin(), d_keys.end(), d_centres.begin());
+
+
     thrust::host_vector<grace::Vector3<float> > h_centres = d_centres;
     thrust::host_vector<float> h_radii = d_radii;
     thrust::host_vector<UInteger32> h_keys = d_keys;
     // for (int i=0; i<N; i++) {
-    //     std::cout << "x: " << std::fixed << std::setw(6) << std::setprecision(6)
+    //     std::cout << "x: " << std::fixed << std::setw(15) << std::setprecision(15)
     //                        << std::setfill('0') << h_centres[i].x << std::endl;
-    //     std::cout << "y: " << std::fixed << std::setw(6) << std::setprecision(6)
+    //     std::cout << "y: " << std::fixed << std::setw(15) << std::setprecision(15)
     //                        << std::setfill('0') << h_centres[i].y << std::endl;
-    //     std::cout << "z: " << std::fixed << std::setw(6) << std::setprecision(6)
+    //     std::cout << "z: " << std::fixed << std::setw(15) << std::setprecision(15)
     //                        << std::setfill('0') << h_centres[i].z << std::endl;
-    //     std::cout << "r: " << std::fixed << std::setw(6) << std::setprecision(6)
+    //     std::cout << "r: " << std::fixed << std::setw(15) << std::setprecision(15)
     //                        << std::setfill('0') << h_radii[i] << std::endl;
     //     std::cout << "Key: " << (std::bitset<32>) h_keys[i] << std::endl;
     //     std::cout << std::endl;
@@ -121,23 +131,32 @@ int main(int argc, char* argv[]) {
 
     thrust::device_vector<grace::Node> d_nodes(N-1);
     thrust::device_vector<grace::Leaf> d_leaves(N);
-    thrust::device_vector<int> d_debug(N-1);
+    // thrust::device_vector<int> d_debug(N-1);
 
     grace::build_nodes(d_nodes, d_leaves, d_keys);
 
-    thrust::host_vector<grace::Node> h_nodes = d_nodes;
-    int parent;
-    for (int i=0; i<N; i++) {
-        parent =  h_nodes[i].parent;
-        if (parent == 0) {
-            std::cout << "i: " << i << std::endl;
-            std::cout << "parent: " << parent << std::endl;
-        }
-    }
+    // thrust::host_vector<grace::Node> h_nodes = d_nodes;
+    // thrust::host_vector<grace::Leaf> h_leaves = d_leaves;
+    // std::cout << "Nodes:\n" << std::endl;
+    // for (int i=0; i<(N-1); i++) {
+    //     std::cout << "i:      " << i << std::endl;
+    //     std::cout << "left leaf flag: " << h_nodes[i].left_leaf_flag << std::endl;
+    //     std::cout << "left:   " << h_nodes[i].left << std::endl;
+    //     std::cout << "right leaf flag: " << h_nodes[i].right_leaf_flag << std::endl;
+    //     std::cout << "right:  " << h_nodes[i].right << std::endl;
+    //     std::cout << "parent: " << h_nodes[i].parent << std::endl;
+    //     std::cout << std::endl;
+    // }
+    // std::cout << "Leaves:\n" << std::endl;
+    // for (int i=0; i<N; i++) {
+    //     std::cout << "i:      " << i << std::endl;
+    //     std::cout << "parent: " << h_leaves[i].parent << std::endl;
+    //     std::cout << std::endl;
+    // }
 
     grace::find_AABBs(d_nodes, d_leaves, d_centres, d_radii);
 
-    thrust::host_vector<int> h_debug = d_debug;
+    // thrust::host_vector<int> h_debug = d_debug;
     // for (int i=0; i<N-1; i++) {
     //     std::cout << "debug: " << h_debug[i] << std::endl;
     // }
---
+++ karras-tree/debug_cuda_builder.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+import morton_keys
+from builder import BinRadixTree, LeafNode
+
+spheres = np.array([
+                   [0.691545367240906, 0.585628449916840, 0.869682788848877,
+                    0.069154538214207],
+                   [0.237461879849434, 0.522250175476074, 0.539603292942047,
+                    0.023746188730001],
+                   [0.275667190551758, 0.730803966522217, 0.638666570186615,
+                    0.027566719800234],
+                   [0.384435445070267, 0.082793198525906, 0.510645270347595,
+                    0.038443546742201],
+                   [0.951158463954926, 0.369021147489548, 0.019436636939645,
+                    0.095115847885609],
+                   [0.203072756528854, 0.525394618511200, 0.323507100343704,
+                    0.020307276397943],
+                   [0.295432031154633, 0.799987137317657, 0.177729815244675,
+                    0.029543204233050],
+                   [0.177435606718063, 0.994355678558350, 0.543971061706543,
+                    0.017743561416864],
+                   [0.676712870597839, 0.607463896274567, 0.888761699199677,
+                    0.067671291530132],
+                   [0.113813042640686, 0.869451284408569, 0.283733487129211,
+                    0.011381304822862]
+                  ])
+
+keys = np.array([int('0b00111100001111100110100010011111', 2),
+                 int('0b00110000001001101011100010011000', 2),
+                 int('0b00110001110010010001111100011110', 2),
+                 int('0b00100001001010000010101010100001', 2),
+                 int('0b00001011001011010110011001100111', 2),
+                 int('0b00010100001101000010111001101011', 2),
+                 int('0b00010011100000111110001101011100', 2),
+                 int('0b00110010011010111011110101000011', 2),
+                 int('0b00111100101010011001110111000110', 2),
+                 int('0b00010110000011111011010001100010', 2)])
+
+binary_tree = BinRadixTree.from_primitives(spheres)
+
+for i,key in enumerate(keys):
+    if key not in binary_tree.keys:
+        print "Python key", i, ":", "{0:032b}".format(binary_tree.keys[i])
+        print "CUDA key  ", i, ":", "{0:032b}".format(key)
+
+print "Nodes:"
+for i in range(len(spheres)-1):
+    print "i:              ", i
+    print "left leaf flag: ", binary_tree.nodes[i].left.is_leaf()
+    print "left:           ", binary_tree.nodes[i].left.index
+    print "right leaf flag:", binary_tree.nodes[i].right.is_leaf()
+    print "right:          ", binary_tree.nodes[i].right.index
+    parent = binary_tree.nodes[i].parent
+    if parent is None:
+        parent = 0
+    else:
+        parent = parent.index
+    print "Parent:         ", parent
+    print
+
+print "Leaves:"
+for i in range(len(spheres)):
+    print "i:     ", i
+    parent = binary_tree.leaves[i].parent
+    if parent is None:
+        parent = 0
+    else:
+        parent = parent.index
+    print "Parent:", parent
+    print
+
+
