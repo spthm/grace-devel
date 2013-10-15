19e4f7565960b4f1d06bafafa3002c06b0c06d25
S Thomson <sp.thomson@hotmail.com>
2013-10-15 13:13:32 +0100

    Edited morton.cuh to reduces differences in 30/63-bit code.

--- karras-tree-CUDA/kernels/morton.cuh
+++ karras-tree-CUDA/kernels/morton.cuh
@@ -7,58 +7,37 @@
 
 namespace grace {
 
-template <typename UInteger>
-__host__ __device__ UInteger32 morton_key_30bit(const UInteger& x,
-                                                const UInteger& y,
-                                                const UInteger& z) {
+// 30-bit keys.
+__host__ __device__ UInteger32 morton_key(const UInteger32& x,
+                                          const UInteger32& y,
+                                          const UInteger32& z) {
     return space_by_two_10bit(z) << 2 | space_by_two_10bit(y) << 1 | space_by_two_10bit(x);
 }
 
-template <typename UInteger>
-__host__ __device__ UInteger64 morton_key_63bit(const UInteger& x,
-                                                const UInteger& y,
-                                                const UInteger& z) {
+// 63-bit keys.
+__host__ __device__ UInteger64 morton_key(const UInteger64& x,
+                                          const UInteger64& y,
+                                          const UInteger64& z) {
     return space_by_two_21bit(z) << 2 | space_by_two_21bit(y) << 1 | space_by_two_21bit(x);
 }
 
 namespace gpu {
 
-template <typename Float>
-__global__ void morton_keys_kernel_30bit(const Float* xs,
-                                         const Float* ys,
-                                         const Float* zs,
-                                         UInteger32* keys,
-                                         const UInteger32 n_keys,
-                                         const Vector3<Float> scale)
+template <typename UInteger, typename Float>
+__global__ void morton_keys_kernel(const Float* xs,
+                                   const Float* ys,
+                                   const Float* zs,
+                                   UInteger* keys,
+                                   const UInteger32 n_keys,
+                                   const Vector3<Float> scale)
 {
     UInteger32 tid = threadIdx.x + blockIdx.x * blockDim.x;
     while (tid < n_keys) {
-        UInteger32 x = (UInteger32) scale.x * xs[tid];
-        UInteger32 y = (UInteger32) scale.y * ys[tid];
-        UInteger32 z = (UInteger32) scale.z * zs[tid];
+        UInteger x = (UInteger) scale.x * xs[tid];
+        UInteger y = (UInteger) scale.y * ys[tid];
+        UInteger z = (UInteger) scale.z * zs[tid];
 
-        keys[tid] = morton_key_30bit(x, y, z);
-
-        tid += blockDim.x * gridDim.x;
-    }
-    return;
-}
-
-template <typename Float>
-__global__ void morton_keys_kernel_63bit(const Float* xs,
-                                         const Float* ys,
-                                         const Float* zs,
-                                         UInteger64* keys,
-                                         const UInteger32 n_keys,
-                                         const Vector3<Float> scale)
-{
-    UInteger64 tid = threadIdx.x + blockIdx.x * blockDim.x;
-    while (tid < n_keys) {
-        UInteger64 x = (UInteger64) scale.x * xs[tid];
-        UInteger64 y = (UInteger64) scale.y * ys[tid];
-        UInteger64 z = (UInteger64) scale.z * zs[tid];
-
-        keys[tid] = morton_key_63bit(x, y, z);
+        keys[tid] = morton_key(x, y, z);
 
         tid += blockDim.x * gridDim.x;
     }
@@ -68,17 +47,16 @@ __global__ void morton_keys_kernel_63bit(const Float* xs,
 } // namespace gpu
 
 template <typename UInteger, typename Float>
-void morton_keys();
-
-template <typename Float>
-void morton_keys<UInteger32, Float>(const thrust::device_vector<Float>& d_xs,
-                                    const thrust::device_vector<Float>& d_ys,
-                                    const thrust::device_vector<Float>& d_zs,
-                                    thrust::device_vector<UInteger32>& d_keys;
-                                    const Vector3<Float>& AABB_bottom,
-                                    const Vector3<Float>& AABB_top)
+void morton_keys(const thrust::device_vector<Float>& d_xs,
+                 const thrust::device_vector<Float>& d_ys,
+                 const thrust::device_vector<Float>& d_zs,
+                 thrust::device_vector<UInteger>& d_keys;
+                 const Vector3<Float>& AABB_bottom,
+                 const Vector3<Float>& AABB_top)
 {
-    unsigned int span = (1u << 10) - 1;
+    // Should be optimized away by the compiler.
+    unsigned int span = CHAR_BIT * sizeof(UInteger) > 32 ?
+                            ((1u << 21) - 1) : ((1u << 10) - 1);
     Vector3<Float> scale((Float)span / (AABB_top.x - AABB_bottom.x)
                          (Float)span / (AABB_top.y - AABB_bottom.y)
                          (Float)span / (AABB_top.z - AABB_bottom.z));
@@ -92,31 +70,7 @@ void morton_keys<UInteger32, Float>(const thrust::device_vector<Float>& d_xs,
                                   / THREADS_PER_BLOCK);
 
     d_keys.resize(n_keys);
-    gpu::morton_keys_kernel_30bit<<<blocks, MAX_THREADS_PER_BLOCK>>>(
-        thrust::raw_pointer_cast(d_xs.data()),
-        thrust::raw_pointer_cast(d_ys.data()),
-        thrust::raw_pointer_cast(d_zs.data()),
-        thrust::raw_pointer_cast(d_keys.data()),
-        n_keys,
-        scale);
-}
-
-template <typename Float>
-void morton_keys<UInteger64, Float>(const thrust::device_vector<Float>& d_xs,
-                                    const thrust::device_vector<Float>& d_ys,
-                                    const thrust::device_vector<Float>& d_zs,
-                                    thrust::device_vector<UInteger64>& d_keys;
-                                    const Vector3<Float>& AABB_bottom,
-                                    const Vector3<Float>& AABB_top)
-{
-    unsigned int span = (1u << 21) - 1;
-    Vector3<Float> scale((Float)span / (AABB_top.x - AABB_bottom.x)
-                         (Float)span / (AABB_top.y - AABB_bottom.y)
-                         (Float)span / (AABB_top.z - AABB_bottom.z));
-    UInteger32 n_keys = d_xs.size();
-
-    d_keys.resize(n_keys);
-    gpu::morton_keys_kernel_63bit<<<blocks, MAX_THREADS_PER_BLOCK>>>(
+    gpu::morton_keys_kernel<<<blocks, MAX_THREADS_PER_BLOCK>>>(
         thrust::raw_pointer_cast(d_xs.data()),
         thrust::raw_pointer_cast(d_ys.data()),
         thrust::raw_pointer_cast(d_zs.data()),
