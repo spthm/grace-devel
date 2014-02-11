f20363d5660e03b3fe28da8bdde9f304e69f4ce6
S Thomson <spth@roe.ac.uk>
2014-02-11 16:34:22 +0000

    Updated basic tree tester to SoA format.  UNTESTED.

--- karras-tree-CUDA/tests/tree_30bit.cu
+++ karras-tree-CUDA/tests/tree_30bit.cu
@@ -126,7 +126,7 @@ int main(int argc, char* argv[]) {
     Vector3f top(1., 1., 1.);
 
     grace::morton_keys(d_x_centres, d_y_centres, d_z_centres,
-                      d_keys, bottom, top);
+                       d_keys, bottom, top);
 
     if (save_out) {
         h_write_uint = d_keys;
@@ -193,53 +193,85 @@ int main(int argc, char* argv[]) {
 
     /* Build the tree from the keys. */
 
-    thrust::device_vector<grace::Node> d_nodes(N-1);
-    thrust::device_vector<grace::Leaf> d_leaves(N);
+    thrust::device_vector<grace::integer32> d_nodes_left(N-1);
+    thrust::device_vector<grace::integer32> d_nodes_right(N-1);
+    thrust::device_vector<grace::integer32> d_nodes_parent(N-1);
+    thrust::device_vector<grace::integer32> d_nodes_end(N-1);
+    thrust::device_vector<unsigned int> d_nodes_level(N-1);
+    thrust::device_vector<float> d_nodes_top(3*(N-1));
+    thrust::device_vector<float> d_nodes_bottom(3*(N-1));
 
-    grace::build_nodes(d_nodes, d_leaves, d_keys);
-    grace::find_AABBs(d_nodes, d_leaves,
+    thrust::device_vector<grace::integer32> d_leaves_parent(N);
+    thrust::device_vector<float> d_leaves_top(3*N);
+    thrust::device_vector<float> d_leaves_bottom(3*N);
+
+    grace::build_nodes(d_nodes_left,
+                       d_nodes_right,
+                       d_nodes_parent,
+                       d_nodes_end,
+                       d_nodes_level,
+                       d_leaves_parent,
+                       d_keys);
+    grace::find_AABBs(d_nodes_left,
+                      d_nodes_right,
+                      d_nodes_parent,
+                      d_nodes_end,
+                      d_nodes_top,
+                      d_nodes_bottom,
+                      d_leaves_parent,
+                      d_leaves_top,
+                      d_leaves_bottom,
                       d_x_centres, d_y_centres, d_z_centres, d_radii);
 
 
     /* Save node and leaf data. */
 
-    thrust::host_vector<grace::Node> h_nodes = d_nodes;
-    thrust::host_vector<grace::Leaf> h_leaves = d_leaves;
+    thrust::host_vector<grace::integer32> h_nodes_left = d_nodes_left;
+    thrust::host_vector<grace::integer32> h_nodes_right = d_nodes_right;
+    thrust::host_vector<grace::integer32> h_nodes_parent = d_nodes_parent;
+    thrust::host_vector<grace::integer32> h_nodes_end = d_nodes_end;
+    thrust::host_vector<unsigned int> h_nodes_level = d_nodes_level;
+    thrust::host_vector<float> h_nodes_top = d_nodes_top;
+    thrust::host_vector<float> h_nodes_bottom = d_nodes_bottom;
+
+    thrust::host_vector<grace::integer32> h_leaves_parent = d_leaves_parent;
+    thrust::host_vector<float> h_leaves_top = d_leaves_top;
+    thrust::host_vector<float> h_leaves_bottom = d_leaves_bottom;
 
     if (save_out) {
         outfile.open("outdata/nodes.txt");
         for (unsigned int i=0; i<N-1; i++) {
             outfile << "i:               " << i << std::endl;
-            outfile << "level:           " << h_nodes[i].level << std::endl;
+            outfile << "level:           " << h_nodes_level[i] << std::endl;
             // Output the actual index into the leaf array for comparison
             // to the Python code.
-            if (h_nodes[i].left > N-2) {
+            if (h_nodes_left[i] > N-2) {
                 outfile << "left leaf flag:  True" << std::endl;
-                outfile << "left:            " << h_nodes[i].left - (N-1)
+                outfile << "left:            " << h_nodes_left[i] - (N-1)
                         << std::endl;
             }
             else {
                 outfile << "left leaf flag:  False" << std::endl;
-                outfile << "left:            " << h_nodes[i].left << std::endl;
+                outfile << "left:            " << h_nodes_left[i] << std::endl;
             }
-            if (h_nodes[i].right > N-2) {
+            if (h_nodes_right[i]> N-2) {
                 outfile << "right leaf flag:  True" << std::endl;
-                outfile << "right:            " << h_nodes[i].right - (N-1)
+                outfile << "right:            " << h_nodes_right[i] - (N-1)
                         << std::endl;
             }
             else {
                 outfile << "right leaf flag:  False" << std::endl;
-                outfile << "right:            " << h_nodes[i].right
+                outfile << "right:            " << h_nodes_right[i]
                         << std::endl;
             }
-            outfile << "parent:          " << h_nodes[i].parent << std::endl;
-            outfile << "AABB_bottom:     " << h_nodes[i].bottom[0] << ", "
-                                           << h_nodes[i].bottom[1] << ", "
-                                           << h_nodes[i].bottom[2]
+            outfile << "parent:          " << h_nodes_parent[i] << std::endl;
+            outfile << "AABB_bottom:     " << h_nodes_bottom[3*i+0] << ", "
+                                           << h_nodes_bottom[3*i+1] << ", "
+                                           << h_nodes_bottom[3*i+2]
                                            << std::endl;
-            outfile << "AABB_top:        " << h_nodes[i].top[0] << ", "
-                                           << h_nodes[i].top[1] << ", "
-                                           << h_nodes[i].top[2] << std::endl;
+            outfile << "AABB_top:        " << h_nodes_top[3*i+0] << ", "
+                                           << h_nodes_top[3*i+1] << ", "
+                                           << h_nodes_top[3*i+2] << std::endl;
             outfile << std::endl;
         }
         outfile.close();
@@ -247,13 +279,13 @@ int main(int argc, char* argv[]) {
         outfile.open("outdata/leaves.txt");
         for (unsigned int i=0; i<N; i++) {
             outfile << "i:           " << i << std::endl;
-            outfile << "parent:      " << h_leaves[i].parent << std::endl;
-            outfile << "AABB_bottom: " << h_leaves[i].bottom[0] << ", "
-                                       << h_leaves[i].bottom[1] << ", "
-                                       << h_leaves[i].bottom[2] << std::endl;
-            outfile << "AABB_top:    " << h_leaves[i].top[0] << ", "
-                                       << h_leaves[i].top[1] << ", "
-                                       << h_leaves[i].top[2] << std::endl;
+            outfile << "parent:      " << h_leaves_parent[i] << std::endl;
+            outfile << "AABB_bottom: " << h_leaves_bottom[3*i+0] << ", "
+                                       << h_leaves_bottom[3*i+1] << ", "
+                                       << h_leaves_bottom[3*i+2] << std::endl;
+            outfile << "AABB_top:    " << h_leaves_top[3*i+0] << ", "
+                                       << h_leaves_top[3*i+1] << ", "
+                                       << h_leaves_top[3*i+2] << std::endl;
             outfile << std::endl;
         }
     }
