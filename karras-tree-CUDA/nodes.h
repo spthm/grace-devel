07c30e42a1b78020a3d1628b7e5fbf4fdb865abc
S Thomson <spth@roe.ac.uk>
2014-02-13 19:32:37 +0000

    Made Nodes/Leaves SoA a class with constructor.  Added host version.

--- karras-tree-CUDA/nodes.h
+++ karras-tree-CUDA/nodes.h
@@ -7,8 +7,9 @@
 
 namespace grace {
 
-struct Nodes
+class Nodes
 {
+public:
     thrust::device_vector<integer32> left;
     thrust::device_vector<integer32> right;
     thrust::device_vector<integer32> parent;
@@ -18,14 +19,56 @@ struct Nodes
 
     thrust::device_vector<float> top;
     thrust::device_vector<float> bot;
+
+    Nodes(unsigned int N_nodes) : left(N_nodes), right(N_nodes),
+                                  parent(N_nodes),
+                                  end(N_nodes),
+                                  level(N_nodes),
+                                  top(3*N_nodes), bot(3*N_nodes) {}
 };
 
-struct Leaves
+class Leaves
 {
+public:
     thrust::device_vector<integer32> parent;
 
     thrust::device_vector<float> top;
     thrust::device_vector<float> bot;
+
+    Leaves(unsigned int N_leaves) : parent(N_leaves),
+                                    top(3*N_leaves), bot(3*N_leaves) {}
+};
+
+class H_Nodes
+{
+public:
+    thrust::host_vector<integer32> left;
+    thrust::host_vector<integer32> right;
+    thrust::host_vector<integer32> parent;
+    thrust::host_vector<integer32> end;
+
+    thrust::host_vector<unsigned int> level;
+
+    thrust::host_vector<float> top;
+    thrust::host_vector<float> bot;
+
+    H_Nodes(unsigned int N_nodes) : left(N_nodes), right(N_nodes),
+                                    parent(N_nodes),
+                                    end(N_nodes),
+                                    level(N_nodes),
+                                    top(3*N_nodes), bot(3*N_nodes) {}
+};
+
+class H_Leaves
+{
+public:
+    thrust::host_vector<integer32> parent;
+
+    thrust::host_vector<float> top;
+    thrust::host_vector<float> bot;
+
+    H_Leaves(unsigned int N_leaves) : parent(N_leaves),
+                                      top(3*N_leaves), bot(3*N_leaves) {}
 };
 
 struct Node
