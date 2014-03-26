55195cdb80222bee779c479423d028103f806ace
S Thomson <spth@roe.ac.uk>
2014-03-26 09:43:24 +0000

    Updated Python tracer.

--- karras-tree/debug_cuda_trace.py
+++ karras-tree/debug_cuda_trace.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from builder import BinRadixTree
+from Nodes import Node
 from trace import Ray
 
 with open("../karras-tree-CUDA/tests/indata/spheredata.txt") as f:
@@ -15,45 +16,38 @@ with open("../karras-tree-CUDA/tests/outdata/hitdata.txt") as f:
 binary_tree = BinRadixTree.from_primitives(spheres)
 
 
-# Trace.
+print "Tracing..."
 hit_counts = np.zeros(len(rays))
 for ray_index in range(len(rays)):
-    trace_stack = np.zeros(31, dtype=int)
-    stack_index = 0
-
-    node_index = 0
-    is_leaf = False
+    trace_stack = [Node.null(), ]
+    node = binary_tree.nodes[0]
     ray_hit_count = 0
 
     ray = rays[ray_index]
-    while (stack_index >= 0):
-
-        while (not is_leaf and stack_index >= 0):
-            if ray.AABB_hit(binary_tree.nodes[node_index].AABB):
-                stack_index += 1
-                trace_stack[stack_index] = node_index
-                is_leaf = binary_tree.nodes[node_index].left.is_leaf()
-                node_index = binary_tree.nodes[node_index].left.index
+    while (not node.is_leaf() and not node.is_null()):
+        while (not node.is_leaf() and not node.is_null()):
+            if ray.AABB_hit(node.AABB):
+                trace_stack.append(node.right)
+                node = node.left
             else:
-                node_index = trace_stack[stack_index]
-                stack_index -= 1
-                is_leaf = binary_tree.nodes[node_index].right.is_leaf()
-                node_index = binary_tree.nodes[node_index].right.index
+                node = trace_stack.pop()
 
-        while (is_leaf and stack_index >= 0):
-            if ray.sphere_hit(*spheres[node_index]):
+        while (node.is_leaf() and not node.is_null()):
+            if ray.sphere_hit(*spheres[node.index]):
                 ray_hit_count += 1
-
-            node_index = trace_stack[stack_index]
-            stack_index -= 1
-            is_leaf = binary_tree.nodes[node_index].right.is_leaf()
-            node_index = binary_tree.nodes[node_index].right.index
+            node = trace_stack.pop()
 
     hit_counts[ray_index] = ray_hit_count
 
+error = False
 print
 for i, n_py_hits in enumerate(hit_counts):
     if n_py_hits != hitdata[i]:
+        error = True
         print "Hit count mismatch at", i+1
         print "Py hits", n_py_hits, "vs", hitdata[i], "CUDA hits!"
         print
+
+if not error:
+    print "All hits match!"
+    print
