f98b6642682c0d0dab86ef17f131a2ac5427f809
S Thomson <sp.thomson@hotmail.com>
2014-05-02 00:09:14 +0100

    Added some helper files for fixing the ray generation.

---
+++ karras-tree-CUDA/tests/curand_normal.cu
@@ -0,0 +1,124 @@
+#include <curand_kernel.h>
+
+#include <fstream>
+
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+
+#define N_DIM_MAX 3
+
+inline void cuda_call(cudaError_t code, char* file, int line, bool abort=true)
+{
+    if (code != cudaSuccess) {
+        fprintf(stderr,"CUDA Error!\nMsg:  %s\nFile: %s @ line %d\n",
+                cudaGetErrorString(code), file, line);
+
+    if (abort)
+        exit(code);
+    }
+}
+
+__global__ void init_QRNG(curandStateSobol32_t *const qrng_states,
+                          curandDirectionVectors32_t *const qrng_directions, //const?
+                          const unsigned int N_dim)
+{
+    unsigned int tid = threadIdx.x + blockIdx.x * blockDim.x;
+
+    // Initialise the Q-RNG(s).
+    for (unsigned int d=0; d<N_dim; d++) {
+        curand_init(qrng_directions[d], tid+2, &qrng_states[N_dim*tid+d]);
+    }
+}
+
+
+__global__ void gen_normals(float* out,
+                            curandStateSobol32_t *const qrng_states,
+                            const size_t N,
+                            const unsigned int N_dim)
+{
+    unsigned int tid = threadIdx.x + blockIdx.x * blockDim.x;
+
+    curandStateSobol32_t local_states[N_DIM_MAX];
+
+    for (unsigned int d=0; d<N_dim; d++) {
+        local_states[d] = qrng_states[N_dim*tid+d];
+    }
+
+    while (tid < N)
+    {
+        float normal;
+        for (unsigned int d=0; d<N_dim; d++) {
+            normal = curand_normal(&local_states[d]);
+            out[N_dim*tid+d] = normal;
+        }
+
+        tid += blockDim.x * gridDim.x;
+    }
+}
+
+int main(int argc, char* argv[]) {
+
+    std::ofstream outfile;
+    outfile.setf(std::ios::fixed, std::ios::floatfield);
+    outfile.precision(9);
+
+    size_t N = 100;
+    unsigned int N_dim = 1;
+
+    if (argc > 1) {
+        N_dim = (unsigned int) std::strtol(argv[1], NULL, 10);
+    }
+    if (argc > 2) {
+        N = (size_t) std::strtol(argv[2], NULL, 10);
+    }
+
+    // Allocate space for Q-RNG states and the direction vector(s).
+    curandStateSobol32_t *d_qrng_states;
+    cudaMalloc((void**)&d_qrng_states, 2*32*N_dim * sizeof(curandStateSobol32_t));
+
+    curandDirectionVectors32_t *d_qrng_directions;
+    cudaMalloc((void**)&d_qrng_directions,
+               N_dim*sizeof(curandDirectionVectors32_t));
+
+    // Generate Q-RNG 'direction vectors' on host, and copy to device.
+    curandDirectionVectors32_t *qrng_directions;
+    curandGetDirectionVectors32(&qrng_directions,
+                                CURAND_DIRECTION_VECTORS_32_JOEKUO6);
+    cudaMemcpy(d_qrng_directions, qrng_directions,
+               N_dim*sizeof(curandDirectionVectors32_t),
+               cudaMemcpyHostToDevice);
+
+    // Initialize QRNG.
+    init_QRNG<<<2, 32>>>(d_qrng_states, d_qrng_directions, N_dim);
+    cuda_call( cudaPeekAtLastError(), __FILE__, __LINE__ );
+    cuda_call( cudaDeviceSynchronize(), __FILE__, __LINE__ );
+
+    // Allocate space for normals and generate them.
+    float *d_out;
+    cudaMalloc((void**)&d_out, N*N_dim*sizeof(float));
+    gen_normals<<<2, 32>>>(d_out, d_qrng_states, N, N_dim);
+    cuda_call( cudaPeekAtLastError(), __FILE__, __LINE__ );
+    cuda_call( cudaDeviceSynchronize(), __FILE__, __LINE__ );
+
+    // Copy back to device.
+    float* h_out = new float[N*N_dim];
+    cudaMemcpy(h_out, d_out, N*N_dim*sizeof(float), cudaMemcpyDeviceToHost);
+
+    std::cout << sizeof(curandDirectionVectors32_t) << std::endl;
+
+    // Write normals to file.
+    outfile.open("normals.txt");
+    for (size_t i=0; i<N; i++) {
+        for (unsigned int d=0; d<N_dim; d++) {
+            outfile << h_out[N_dim*i+d] << " ";
+            if (h_out[N_dim*i+d] > 0)
+                outfile << " ";
+        }
+        outfile << std::endl;
+    }
+    outfile.close();
+
+    cudaFree(d_qrng_states);
+    cudaFree(d_qrng_directions);
+    cudaFree(d_out);
+}
---
+++ karras-tree-CUDA/tests/curand_uniform.cu
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <curand.h>
+#include <curand_kernel.h>
+
+// QRNG init kernel
+__global__ void init_QRNG(curandStateSobol32_t *const rngStates,
+                          curandDirectionVectors32_t *const rngDirections)
+{
+    unsigned int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned int step = gridDim.x * blockDim.x;
+
+    curand_init(rngDirections[0], tid, &rngStates[tid]);
+    curand_init(rngDirections[1], tid, &rngStates[tid + step]);
+}
+
+__global__ void generate_uniforms(float *const results,
+                                  curandStateSobol32_t *const rngStates,
+                                  const unsigned int N)
+{
+    unsigned int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned int step = gridDim.x * blockDim.x;
+
+    // Copy state to local memory for efficiency.
+    curandStateSobol32_t localState1 = rngStates[tid];
+    curandStateSobol32_t localState2 = rngStates[tid + step];
+
+    // Generate up to N random points and save each one.
+    for (unsigned int i=tid ; i<N ; i+=step)
+    {
+        float x = curand_uniform(&localState1);
+        float y = curand_uniform(&localState2);
+
+        results[2*i+0] = x;
+        results[2*i+1] = y;
+    }
+}
+
+int main(void) {
+
+    dim3 block;
+    dim3 grid;
+
+    // The number of random points we wish to generate.
+    unsigned int N = 16;
+
+    // A maximum/optimum for our hypothetical device.
+    unsigned int threadBlockSize = 4;
+
+    block.x = threadBlockSize;
+    // Grid size required to cover all work with unique threads.
+    grid.x  = (N + threadBlockSize - 1) / threadBlockSize;
+
+    // Reduced grid size that fits our hypothetical device.
+    grid.x = 2;
+
+    // Allocate memory for RNG states and direction vectors.
+    // Factor of two since each thread generates independent x and y values.
+    curandStateSobol32_t       *d_rngStates     = 0;
+    curandDirectionVectors32_t *d_rngDirections = 0;
+    cudaMalloc((void **)&d_rngStates,
+               2 * grid.x * block.x * sizeof(curandStateSobol32_t));
+    cudaMalloc((void **)&d_rngDirections,
+               2 * sizeof(curandDirectionVectors32_t));
+
+    // Allocate memory for result
+    float *d_results = 0;
+    cudaMalloc((void **)&d_results, 2 * N * sizeof(float));
+
+    // Generate direction vectors on the host and copy to the device.
+    curandDirectionVectors32_t *rngDirections;
+    curandGetDirectionVectors32(&rngDirections,
+                                CURAND_DIRECTION_VECTORS_32_JOEKUO6);
+    cudaMemcpy(d_rngDirections, rngDirections,
+               2 * sizeof(curandDirectionVectors32_t), cudaMemcpyHostToDevice);
+
+    // Generate N points from 2*N quasirandom numbers.
+    init_QRNG<<<grid, block>>>(d_rngStates, d_rngDirections);
+    generate_uniforms<<<grid, block>>>(d_results, d_rngStates, N);
+
+    // Allocate memory on host for floats and copy from device.
+    float* h_results = new float[2*N];
+    cudaMemcpy(h_results, d_results,
+               2 * N * sizeof(float), cudaMemcpyDeviceToHost);
+
+    // Print results.
+    // Note that lines 9-15 of the output are idential to lines 2-8!
+    std::cout.setf(std::ios::fixed, std::ios::floatfield);
+    std::cout.precision(5);
+    for (unsigned int i=0; i<N; i++) {
+        std::cout << "x: " << h_results[2*i+0] << ", y: " << h_results[2*i+1]
+                  << std::endl;
+    }
+
+    cudaFree(d_rngStates);
+    cudaFree(d_rngDirections);
+    cudaFree(d_results);
+    delete [] h_results;
+
+    return 0;
+}
