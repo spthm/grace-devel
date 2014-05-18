27d20c123c408edf1a35bdc444865ef5b7c0bcfd
Sam Thomson <sp.thomson@outlook.com>
2014-05-18 14:00:12 +0100

    More changes to ion_particle type.

--- karras-tree-CUDA/ion_particle.h
+++ karras-tree-CUDA/ion_particle.h
@@ -4,10 +4,7 @@ namespace grace {
 
 struct ion_particle
 {
-    // unsigned int id;
-    // If we need ID, can instead store mass_H and mass_He. Then this struct
-    // still divides evenly into (float)4s.
-    float mass, frac_H, frac_He;
+    float n;
     float x_HI, x_HeI, x_HeII;
     float gamma_HI, gamma_HeI, gamma_HeII;
     float H_HI, H_HeI, H_HeII;
