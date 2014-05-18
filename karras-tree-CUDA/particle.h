fecebc8a2d88c5fb75a862a436d9ed58957547fe
Sam Thomson <sp.thomson@outlook.com>
2014-05-18 13:20:51 +0100

    Introduced an ionized particle type.

--- karras-tree-CUDA/particle.h
+++ karras-tree-CUDA/particle.h
@@ -2,13 +2,15 @@
 
 namespace grace {
 
-struct particle
+struct ion_particle
 {
-    float4 xyzr;
-    unsigned int id;
-    float rho;
+    // unsigned int id;
+    // If we need ID, can instead store mass_H and mass_He. Then this struct
+    // still divides evenly into (float)4s.
+    float mass, frac_H, frac_He;
+    float x_HI, x_HeI, x_HeII;
     float gamma_HI, gamma_HeI, gamma_HeII;
-    float sigma_HI, sigma_HeI, sigma_HeII;
+    float H_HI, H_HeI, H_HeII;
 };
 
 } // namespace grace
