8b69019394d1e7c433daf9cfc0c85a009e29acad
S Thomson <spth@roe.ac.uk>
2015-01-15 18:00:44 +0000

    Updated Makefile from MGPU support. kernel_config modified to remove MGPU conflicts. (MGPU uses a WARP_SIZE variable, so macros changed to const ints.)

--- karras-tree-CUDA/kernel_config.h
+++ karras-tree-CUDA/kernel_config.h
@@ -1,10 +1,14 @@
 #pragma once
 
-#define MORTON_THREADS_PER_BLOCK 512
-#define BUILD_THREADS_PER_BLOCK 512
-#define SHIFTS_THREADS_PER_BLOCK 512
-#define AABB_THREADS_PER_BLOCK 512
-#define TRACE_THREADS_PER_BLOCK 256
-#define RAYS_THREADS_PER_BLOCK 512
-#define MAX_BLOCKS 112 // 7MPs * 16 blocks/MP for compute capability 3.0.
-#define WARP_SIZE 32
+namespace grace {
+
+const int MORTON_THREADS_PER_BLOCK = 512;
+const int BUILD_THREADS_PER_BLOCK = 512;
+const int SHIFTS_THREADS_PER_BLOCK = 512;
+const int AABB_THREADS_PER_BLOCK = 512;
+const int TRACE_THREADS_PER_BLOCK = 256;
+const int RAYS_THREADS_PER_BLOCK = 512;
+const int MAX_BLOCKS = 112; // 7MPs * 16 blocks/MP for compute capability 3.0.
+const int WARP_SIZE = 32;
+
+} // namespace grace
