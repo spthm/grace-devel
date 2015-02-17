6ae5447260e3937eb3c1a2504c895b83ec70100f
Sam Thomson <sp.thomson@hotmail.com>
2015-02-17 13:11:04 +0000

    Changed L2 load/stores in build_leaves to normal load/stores. Also added statements to prevent unwanted memory optimizations. No measureable effect on performance.

--- karras-tree-CUDA/kernels/bintree_build.cuh
+++ karras-tree-CUDA/kernels/bintree_build.cuh
@@ -120,14 +120,17 @@ __device__ float node_delta(const int i,
 // globally coherent.
 // All take a pointer with the type of the base primitive (i.e. int* for int 2
 // read/writes, float* for float4 read/writes) for flexibility.
-__device__ __forceinline__ int2 load_L2_vec2s32(const int* const addr)
+// The "memory" clobber is added to all load/store PTX instructions to prevent
+// memory optimizations around the asm statements. We should only be using these
+// functions when we know better than the compiler!
+__device__ __forceinline__ int2 load_vec2s32(const int* const addr)
 {
     int2 i2;
 
     #if defined(__LP64__) || defined(_WIN64)
-    asm("ld.global.cg.v2.s32 {%0, %1}, [%2];" : "=r"(i2.x), "=r"(i2.y) : "l"(addr));
+    asm("ld.global.ca.v2.s32 {%0, %1}, [%2];" : "=r"(i2.x), "=r"(i2.y) : "l"(addr) : "memory");
     #else
-    asm("ld.global.cg.v2.s32 {%0, %1}, [%2];" : "=r"(i2.x), "=r"(i2.y) : "r"(addr));
+    asm("ld.global.ca.v2.s32 {%0, %1}, [%2];" : "=r"(i2.x), "=r"(i2.y) : "r"(addr) : "memory");
     #endif
 
     return i2;
@@ -138,21 +141,22 @@ __device__ __forceinline__ float4 load_L2_vec4f32(const float* const addr)
     float4 f4;
 
     #if defined(__LP64__) || defined(_WIN64)
-    asm("ld.global.cg.v4.f32 {%0, %1, %2, %3}, [%4];" : "=f"(f4.x), "=f"(f4.y), "=f"(f4.z), "=f"(f4.w) : "l"(addr));
+    asm("ld.global.cg.v4.f32 {%0, %1, %2, %3}, [%4];" : "=f"(f4.x), "=f"(f4.y), "=f"(f4.z), "=f"(f4.w) : "l"(addr) : "memory");
     #else
-    asm("ld.global.cg.v4.f32 {%0, %1, %2, %3}, [%4];" : "=f"(f4.x), "=f"(f4.y), "=f"(f4.z), "=f"(f4.w) : "r"(addr));
+    asm("ld.global.cg.v4.f32 {%0, %1, %2, %3}, [%4];" : "=f"(f4.x), "=f"(f4.y), "=f"(f4.z), "=f"(f4.w) : "r"(addr) : "memory");
     #endif
 
     return f4;
 }
 
-__device__ __forceinline__ void store_L2_s32(
-    const int* const addr, const int a)
+// Stores have no output operands, so we additionally mark them as volatile to
+// ensure they are not moved or deleted.
+__device__ __forceinline__ void store_s32(const int* const addr, const int a)
 {
     #if defined(__LP64__) || defined(_WIN64)
-    asm("st.global.cg.s32 [%0], %1;" :: "l"(addr), "r"(a));
+    asm volatile ("st.global.wb.s32 [%0], %1;" :: "l"(addr), "r"(a) : "memory");
     #else
-    asm("st.global.cg.s32 [%0], %1;" :: "r"(addr), "r"(a));
+    asm volatile ("st.global.wb.s32 [%0], %1;" :: "r"(addr), "r"(a) : "memory");
     #endif
 }
 
@@ -161,9 +165,9 @@ __device__ __forceinline__ void store_L2_vec4f32(
     const float a, const float b, const float c, const float d)
 {
     #if defined(__LP64__) || defined(_WIN64)
-    asm("st.global.cg.v4.f32 [%0], {%1, %2, %3, %4};" :: "l"(addr), "f"(a), "f"(b), "f"(c), "f"(d));
+    asm volatile ("st.global.cg.v4.f32 [%0], {%1, %2, %3, %4};" :: "l"(addr), "f"(a), "f"(b), "f"(c), "f"(d) : "memory");
     #else
-    asm("st.global.cg.v4.f32 [%0], {%1, %2, %3, %4};" :: "r"(addr), "f"(a), "f"(b), "f"(c), "f"(d));
+    asm volatile ("st.global.cg.v4.f32 [%0], {%1, %2, %3, %4};" :: "r"(addr), "f"(a), "f"(b), "f"(c), "f"(d) : "memory");
     #endif
 }
 
@@ -290,12 +294,9 @@ __global__ void build_leaves_kernel(int2* nodes,
             if (parent_index < low || parent_index >= high)
                 continue;
 
-            // The next thread to read this data may be in a different block,
-            // and this always has to be assumed. Hence, L1 cache (which is not
-            // globally coherent) is essentially useless. L2, however, is
-            // globally coherent.
-            // Store DIRECTLY to L2.
-            store_L2_s32(addr, end);
+            // Normal store.  Other threads in this block can read from L1 if
+            // they get a hit.  No requirement for global coherency.
+            store_s32(addr, end);
 
             // Travel up the tree.  The second thread to reach a node writes its
             // left or right end to its parent.  The first exits the loop.
@@ -310,9 +311,10 @@ __global__ void build_leaves_kernel(int2* nodes,
             {
                 cur_index = parent_index;
 
-                int2 left_right;
-                // Load DIRECTLY from L2. Vector int2 load.
-                left_right = load_L2_vec2s32(&(nodes[parent_index].x));
+                // We are certain that a thread in this block has already
+                // written the other child of the current node, so we can read
+                // from L1 cache if we get a hit.  Normal vector int2 load.
+                int2 left_right = load_vec2s32(&(nodes[parent_index].x));
                 int left = left_right.x;
                 int right = left_right.y;
 
@@ -359,8 +361,8 @@ __global__ void build_leaves_kernel(int2* nodes,
                     break;
                 }
 
-                // Store DIRECTLY to L2.
-                store_L2_s32(addr, end);
+                // Normal store.
+                store_s32(addr, end);
 
                 __threadfence_block();
                 assert(parent_index - low >= 0);
