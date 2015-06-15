1e13c76e1b73f2775ad7640a17bd7bc98be79bcd
Sam Thomson <sp.thomson@hotmail.com>
2015-06-15 16:18:56 +0100

    Porint ncols.cuh from eric branch. Updating host functions to be inlined.

--- karras-tree-CUDA/RT/kernels/ncol.cuh
+++ karras-tree-CUDA/RT/kernels/ncol.cuh
@@ -1,59 +1,28 @@
 #include <thrust/device_vector.h>
 #include <thrust/scan.h>
 
+#include "../../types.h"
 #include "../../particle.h"
 
 namespace grace {
 
 namespace gpu {
 
-__global__ void fill_ncols_HI(const particle_ion* particles,
-                              const Float* integrals,
-                              const unsigned int* indices,
-                              const Float N_H,
-                              Float* Ncols,
-                              size_t N_intersections)
+template <typename Float>
+__global__ void multiply_by_weights(const Float* unweighted,
+                                    const Float* weights,
+                                    const unsigned int* weight_map,
+                                    size_t N_unweighted,
+                                    Float* weighted)
 {
     unsigned int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    unsigned int particle_index;
+    unsigned int weight_index;
 
-    while (tid < N_intersections)
+    while (tid < N_unweighted)
     {
-        particle_index = indices[tid];
-        Ncols[tid] = N_H * particles[particle_index].x_HI * integrals[tid];
-        tid += blockDim.x * gridDim.x
-    }
-}
-
-__global__ void fill_ncols_HeI(const particle_ion* particles,
-                               const Float* integrals,
-                               const unsigned int* indices,
-                               const Float N_He,
-                               Float* Ncols,
-                               size_t N_intersections)
-{
-    unsigned int tid = threadIdx.x + blockIdx.x * blockDim.x;
-
-    while (tid < N_intersections)
-    {
-        Ncols[tid] = N_He * particles[tid].x_HeI * integrals[tid];
-        tid += blockDim.x * gridDim.x
-    }
-}
-
-__global__ void fill_ncols_HeII(const particle_ion* particles,
-                                const Float* integrals,
-                                const unsigned int* indices,
-                                const Float N_He,
-                                Float* Ncols,
-                                size_t N_intersections)
-{
-    unsigned int tid = threadIdx.x + blockIdx.x * blockDim.x;
-
-    while (tid < N_intersections)
-    {
-        Ncols[tid] = N_He * particles[tid].x_HeII * integrals[tid];
-        tid += blockDim.x * gridDim.x
+        weight_index = weight_map[tid];
+        weighted[tid] = weights[weight_index] * unweighted[tid];
+        tid += blockDim.x * gridDim.x;
     }
 }
 
@@ -61,67 +30,31 @@ __global__ void fill_ncols_HeII(const particle_ion* particles,
 
 
 template <typename Float>
-void cum_ncol_HI(const thrust::device_vector<particle_ion>& d_particles,
-                 const thrust::device_vector<Float> d_integrals,
-                 const thrust::device_vector<unsigned int>& d_indices,
-                 const thrust::device_vector<unsigned int>& d_ray_segments,
-                 const Float N_H_per_particle,
-                 thrust::device_vector<Float>& d_Ncol)
+GRACE_HOST void cum_weighted_sum(
+    const thrust::device_vector<Float>& d_to_sum,
+    const thrust::device_vector<Float>& d_weights,
+    const thrust::device_vector<unsigned int>& d_weight_map,
+    const thrust::device_vector<int>& d_segment_offsets,
+    thrust::device_vector<Float>& d_sum)
 {
-    // Initialize d_Ncol with the column densities through each particle
-    // (not cumulative!)
-    gpu::fill_ncols_HI<<<48,512>>>(
-        thrust::raw_pointer_cast(d_particles.data()),
-        thrust::raw_pointer_cast(d_integrals.data()),
-        thrust::raw_pointer_cast(d_indices.data()),
-        N_H_per_particle,
-        thrust::raw_pointer_cast(d_Ncol.data()),
-        d_indices.size())
+    // Initialize d_sum with the weighted inputs
+    // (not cumulative!) such that
+    //     weighted[i] = weights[weight_map[i]] * to_sum[i].
+    // e.g.
+    //     Ncol[i] = Ncol_factor[particle_indices[i]] * kernel_integrals[i]
+    thrust::device_vector<Float> d_weighted(d_to_sum.size());
+    gpu::multiply_by_weights<<<48, 512>>>(
+        thrust::raw_pointer_cast(d_to_sum.data()),
+        thrust::raw_pointer_cast(d_weights.data()),
+        thrust::raw_pointer_cast(d_weight_map.data()),
+        d_weighted.size(),
+        thrust::raw_pointer_cast(d_weighted.data()));
 
-    // Sum such that d_Ncol[i] = cumulative Ncol *up to* (not including) the
-    // ith intersected particle.
-    thrust::exclusive_scan_by_key(d_ray_segments.begin(), d_ray_segments.end(),
-                                  d_Ncol.begin(), d_Ncol.begin(), 0.f);
-}
-
-template <typename Float>
-void cum_ncol_HeI(const thrust::device_vector<particle_ion>& d_particles,
-                  const thrust::device_vector<Float> d_integrals,
-                  const thrust::device_vector<unsigned int>& d_indices,
-                  const thrust::device_vector<unsigned int>& d_ray_segments,
-                  const Float N_He_per_particle,
-                  thrust::device_vector<Float>& d_Ncol)
-{
-    gpu::fill_ncols_HeI<<<48,512>>>(
-        thrust::raw_pointer_cast(d_particles.data()),
-        thrust::raw_pointer_cast(d_integrals.data()),
-        thrust::raw_pointer_cast(d_indices.data()),
-        N_He_per_particle,
-        thrust::raw_pointer_cast(d_Ncol.data()),
-        d_indices.size())
-
-    thrust::exclusive_scan_by_key(d_ray_segments.begin(), d_ray_segments.end(),
-                                  d_Ncol.begin(), d_Ncol.begin(), 0.f);
-}
-
-template <typename Float>
-void cum_ncol_HeII(const thrust::device_vector<particle_ion>& d_particles,
-                   const thrust::device_vector<Float> d_integrals,
-                   const thrust::device_vector<unsigned int>& d_indices,
-                   const thrust::device_vector<unsigned int>& d_ray_segments,
-                   const Float N_He_per_particle,
-                   thrust::device_vector<Float>& d_Ncol)
-{
-    gpu::fill_ncols_HeII<<<48,512>>>(
-        thrust::raw_pointer_cast(d_particles.data()),
-        thrust::raw_pointer_cast(d_integrals.data()),
-        thrust::raw_pointer_cast(d_indices.data()),
-        N_He_per_particle,
-        thrust::raw_pointer_cast(d_Ncol.data()),
-        d_indices.size())
-
-    thrust::exclusive_scan_by_key(d_ray_segments.begin(), d_ray_segments.end(),
-                                  d_Ncol.begin(), d_Ncol.begin(), 0.f);
+    // Segmented, cumulative sum such that d_sum[i] = cumulative sum *up to*
+    // (not including, i.e. an exclusive sum) the ith weighted value.
+    // E.g. Ncols[i] = cumulative Ncol *up to* (not including) the ith
+    // intersected particle, for each ray marked by the segments.
+    grace::exclusive_segmented_scan(d_segment_offsets, d_weighted, d_sum);
 }
 
 } // namespace grace
