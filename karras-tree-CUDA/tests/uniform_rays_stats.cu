35a78b998678f764773bfa97df5dc695a97dbcad
Sam Thomson <sp.thomson@hotmail.com>
2015-11-10 16:46:23 +0000

    Improved output for uniform_rays_stats test. Also tidied up the code a little.

--- karras-tree-CUDA/tests/uniform_rays_stats.cu
+++ karras-tree-CUDA/tests/uniform_rays_stats.cu
@@ -11,29 +11,21 @@
 #include "../kernels/gen_rays.cuh"
 #include "../ray.h"
 
-template <typename Real>
-__global__ void fill_XYZs(grace::Ray* rays,
-                          Real* Xs,
-                          Real* Ys,
-                          Real* Zs,
-                          size_t N)
+struct raydir_add : public thrust::binary_function<grace::Ray, grace::Ray, grace::Ray>
 {
-    int tid = threadIdx.x + blockIdx.x * blockDim.x;
-
-    if (tid < N) {
-        grace::Ray ray = rays[tid];
-        Xs[tid] = ray.dx;
-        Ys[tid] = ray.dy;
-        Zs[tid] = ray.dz;
+    __host__ __device__
+    grace::Ray operator()(grace::Ray lhs, grace::Ray rhs) const
+    {
+        grace::Ray ray;
+        ray.dx = lhs.dx + rhs.dx;
+        ray.dy = lhs.dy + rhs.dy;
+        ray.dz = lhs.dz + rhs.dz;
+        return ray;
     }
-
-    return;
-}
+};
 
 template <typename Real>
-__global__ void compute_ij_pairs(Real* Xs,
-                                 Real* Ys,
-                                 Real* Zs,
+__global__ void compute_ij_pairs(grace::Ray* rays,
                                  Real* cos_ijs,
                                  Real* psi_ijs,
                                  Real* sin_ijs,
@@ -45,13 +37,13 @@ __global__ void compute_ij_pairs(Real* Xs,
     unsigned int tid = threadIdx.x + blockIdx.x * blockDim.x;
 
     if (tid < N) {
-        Real xi = Xs[i];
-        Real yi = Ys[i];
-        Real zi = Zs[i];
+        Real xi = rays[i].dx;
+        Real yi = rays[i].dy;
+        Real zi = rays[i].dz;
 
-        Real xj = Xs[j_start + tid];
-        Real yj = Ys[j_start + tid];
-        Real zj = Zs[j_start + tid];
+        Real xj = rays[j_start + tid].dx;
+        Real yj = rays[j_start + tid].dy;
+        Real zj = rays[j_start + tid].dz;
 
         Real cosij = xi*xj + yi*yj + zi*zj;
         cosij = max(-1., min(1., cosij));
@@ -68,13 +60,21 @@ __global__ void compute_ij_pairs(Real* Xs,
     return;
 }
 
+/*
+ * For a desciption of the below statistical tests, see e.g.
+ * "Statistical Analysis of Spherical Data" (1987) by N. I. Fisher, T. Lewis and
+ * B. J. J. Embleton,  Cambridge University Press, Online ISBN:9780511623059.
+ * Chapter 5.3 covers 'R^2 and W statistics, while 5.6 covers An, Gn and Fn
+ * statistics.
+ */
+
 int main(int argc, char* argv[]) {
 
     double PI = 3.14159265358979323846;
     // Precision for statistical calculations.
     // Significant memory requirements, so float may be preferable depending
     // on the device.
-    typedef double Real;
+    typedef float Real;
 
     size_t N_rays = 32 * 300; // = 9600
     bool save_out = false;
@@ -88,47 +88,42 @@ int main(int argc, char* argv[]) {
     }
 
     thrust::device_vector<grace::Ray> d_rays(N_rays);
-    thrust::host_vector<grace::Ray> h_rays(N_rays);
 
     float ox, oy, oz;
     ox = oy = oz = 0;
     float length = 1;
     grace::uniform_random_rays(d_rays, ox, oy, oz, length);
 
-    h_rays = d_rays;
-
     std::ofstream outfile;
     outfile.setf(std::ios::fixed, std::ios::floatfield);
     outfile.precision(6);
 
-    thrust::device_vector<Real> d_X(N_rays);
-    thrust::device_vector<Real> d_Y(N_rays);
-    thrust::device_vector<Real> d_Z(N_rays);
+    grace::Ray zero_ray;
+    zero_ray.dx = zero_ray.dy = zero_ray.dx = 0.f;
+    grace::Ray raydir_sum = thrust::reduce(d_rays.begin(), d_rays.end(),
+                                           zero_ray, raydir_add());
 
-    int num_blocks = (N_rays + 128 - 1) / 128;
-    fill_XYZs<<<num_blocks, 128>>>(
-        thrust::raw_pointer_cast(d_rays.data()),
-        thrust::raw_pointer_cast(d_X.data()),
-        thrust::raw_pointer_cast(d_Y.data()),
-        thrust::raw_pointer_cast(d_Z.data()),
-        N_rays);
-    GRACE_KERNEL_CHECK();
-
-    Real X_sum = thrust::reduce(d_X.begin(), d_X.end(), 0.);
-    Real Y_sum = thrust::reduce(d_Y.begin(), d_Y.end(), 0.);
-    Real Z_sum = thrust::reduce(d_Z.begin(), d_Z.end(), 0.);
-
-    std::cout << "(X sum: " << X_sum << ")" << std::endl;
-    std::cout << "(Y sum: " << Y_sum << ")" << std::endl;
-    std::cout << "(Z sum: " << Z_sum << ")" << std::endl;
+    std::cout << "(X sum: " << raydir_sum.dx << ")" << std::endl;
+    std::cout << "(Y sum: " << raydir_sum.dy << ")" << std::endl;
+    std::cout << "(Z sum: " << raydir_sum.dz << ")" << std::endl;
     std::cout << std::endl;
 
-    Real R2 = X_sum*X_sum + Y_sum*Y_sum + Z_sum*Z_sum;
+    Real R2 = raydir_sum.dx * raydir_sum.dx +
+              raydir_sum.dy * raydir_sum.dy +
+              raydir_sum.dz * raydir_sum.dz;
     Real W = 3. * R2 / N_rays;
     Real W_alt = (1. - (1. / (2.*N_rays)))*W + (1. / (10.*N_rays)) * W*W;
 
     std::cout << "W is:  " << W << std::endl;
     std::cout << "W* is: " << W_alt << std::endl;
+    std::cout << "W < (Chi_3)^2 (0.05) = 7.815 -> it is reasonable to assume"
+              << std::endl
+              << "directions are isotropic against a unimodal alternative"
+              << std::endl
+              << "(there is no evidence at the 5% level to support the unimodal"
+              << std::endl
+              << "alternative)."
+              << std::endl;
     std::cout << std::endl;
 
     // SUM(i,j = 1 TO i,j = N; i != j) = (1/2)n(n-1), and n(n-1) is always even.
@@ -144,9 +139,7 @@ int main(int argc, char* argv[]) {
 
         size_t num_blocks = (N_pairs + 128 - 1) / 128;
         compute_ij_pairs<<<num_blocks, 128>>>(
-            thrust::raw_pointer_cast(d_X.data()),
-            thrust::raw_pointer_cast(d_Y.data()),
-            thrust::raw_pointer_cast(d_Z.data()),
+            thrust::raw_pointer_cast(d_rays.data()),
             thrust::raw_pointer_cast(cospsi_ij.data()),
             thrust::raw_pointer_cast(psi_ij.data()),
             thrust::raw_pointer_cast(sinpsi_ij.data()),
@@ -165,17 +158,37 @@ int main(int argc, char* argv[]) {
     double An = N_rays - coeff * psi_ij_sum;
     double Gn = N_rays / 2. - coeff * sinpsi_ij_sum;
 
-    std::cout << "((4/Npi)*psi_ij.sum() is:    " << coeff * psi_ij_sum << ")"
-              << std::endl;
-    std::cout << "((4/Npi)*sinpsi_ij.sum() is: " << coeff * sinpsi_ij_sum << ")"
-              << std::endl;
-    std::cout << std::endl;
-
     std::cout << "An is: " << An << std::endl;
     std::cout << "Gn is: " << Gn << std::endl;
     std::cout << "Fn is: " << An + Gn << std::endl;
+    std::cout << std::endl;
+    std::cout << "An is Beran's statistic, testing for uniformity against"
+              << std::endl
+              << "alternative models which are not symmetric wrt. the sphere's"
+              << std::endl
+              << "centre." << std::endl;
+    std::cout << "An < 1.413 -> no evidence to reject the isotropic model at"
+              << std::endl
+              << "the 20% level."
+              << std::endl;
+    std::cout << "Gn is Gine's statistic, testing for uniformity against"
+              << std::endl
+              << "alternative models which are symmetric wrt. the sphere's"
+              << std::endl
+              << "centre." << std::endl;
+    std::cout << "Gn < 0.646 -> no evidence to reject the isotropic model at"
+              << std::endl
+              << "the 20% level."
+              << std::endl;
+    std::cout << "Fn tests for uniformity against all alternative models."
+              << std::endl;
+    std::cout << "Fn < 1.948 -> no evidence to reject the isotropic model at"
+              << std::endl
+              << "the 20% level."
+              << std::endl;
 
     if (save_out) {
+        thrust::host_vector<grace::Ray> h_rays = d_rays;
         outfile.open("outdata/ray_dirs.txt");
         for (unsigned int i = 0; i < N_rays; i++) {
             grace::Ray ray = h_rays[i];
