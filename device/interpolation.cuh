3a1ce7839b12bc47257032b2c5598bcfecd3bdf1
Sam Thomson <sp.thomson@outlook.com>
2015-12-18 10:04:15 +0000

    Revert to 1 FMAD and 1 FSUB for linear interpolation. Actually more accurate in this specific case.

--- device/interpolation.cuh
+++ device/interpolation.cuh
@@ -20,8 +20,14 @@ GRACE_DEVICE Real lerp(Real x, TableIter table, int N_table)
     TableReal y1 = table[x_idx + 1];
     TableReal t = static_cast<TableReal>(x) - x_idx;
 
-    // y = (y1 - y0) * t + y0 = t * y1 - t * y0 + y0
-    Real y = fma(t, y1, fma(-t, y0, y0));
+    // y = t * (y1 - y0) + y0 = t * y1 - t * y0 + y0
+    // In the general case, the has error <~ 1 ulp
+    // Real y = fma(t, y1, fma(-t, y0, y0));
+
+    // Sterbenz lemma: floating-point a - b is exact for 0.5*a <= b <= 2*a
+    // This typically holds for adjacent values in a lookup table, so we choose
+    // the below form over the above. It has error 0.5 ulp (error of FMA).
+    Real y = fma(t, y1 - y0, y0);
     return y;
 }
 
