0eb6c95016c4a17dbb670e32ae12815ac865421c
Sam Thomson <sp.thomson@outlook.com>
2016-03-21 23:17:58 +0000

    Added on-GPU generate of plane-parallel rays. Implements #3.

--- kernels/gen_rays.cuh
+++ kernels/gen_rays.cuh
@@ -7,6 +7,7 @@
 
 #include "sort.cuh"
 #include "../device/morton.cuh"
+#include "../device/vecmath.cuh"
 #include "../error.h"
 #include "../kernel_config.h"
 #include "../ray.h"
@@ -145,6 +146,42 @@ __global__ void gen_uniform_rays_single_octant(
         rays[tid] = ray;
 
         tid += blockDim.x * gridDim.x;
+    }
+}
+
+template <typename Real, typename Real3>
+__global__ void plane_parallel_rays_kernel(
+    const int width,
+    const int height,
+    const size_t n_rays,
+    const Real3 base_centre,
+    const Real3 delta_w,
+    const Real3 delta_h,
+    const Real length,
+    const Real3 direction,
+    Ray* const rays)
+{
+    GRACE_ASSERT(grace::WARP_SIZE == warpSize);
+
+    for (int tid = threadIdx.x + blockIdx.x * blockDim.x;
+         tid < n_rays;
+         tid += blockDim.x * gridDim.x)
+    {
+        const int i = tid % width;
+        const int j = tid / width;
+
+        Ray ray;
+        ray.dx = direction.x;
+        ray.dy = direction.y;
+        ray.dz = direction.z;
+
+        ray.ox = base_centre.x + i * delta_w.x + j * delta_h.x;
+        ray.oy = base_centre.y + i * delta_w.y + j * delta_h.y;
+        ray.oz = base_centre.z + i * delta_w.z + j * delta_h.z;
+
+        ray.length = length;
+
+        rays[tid] = ray;
     }
 }
 
@@ -297,11 +334,116 @@ GRACE_HOST void uniform_random_rays_single_octant(
     const enum Octants octant = PPP)
 {
     Ray* const d_rays_ptr = thrust::raw_pointer_cast(d_rays.data());
-    size_t N_rays = d_rays.size();
+    const size_t N_rays = d_rays.size();
 
     uniform_random_rays_single_octant(d_rays_ptr, N_rays, ox, oy, oz, length,
                                       seed, octant);
 
+}
+
+// width and height: the dimensions of the grid of rays to generate
+// base: a point at one corner of the ray-grid plane
+// w: a vector defining the width of the plane, beginning from base
+//    n = width rays are generated along w from base, for m = height rows
+// h: a vector defining the height of the plane, beginning from base
+//    m = height rays are generated along h from base, for n = width columns
+// length: the length of all rays
+//
+// A grid of width * height cells, covering an area |w| * |h|, is produced.
+// The centre of each cell in this grid defines a ray origin. base is located at
+// one corner of this grid. Hence, no ray will have origin == base.
+// The direction of all rays is normalize(cross(w, h)).
+//
+// For rays originating at z = 10, travelling in the -z direction, and covering
+// an area |w| * |h| = 5 * 6 = 30:
+// base = (5, 0, 10)
+// w = (-5, 0, 0)
+// h = (0, 6, 0)
+// direction = normalize(cross(w, h)) = normalize((0, 0, -30)) = (0, 0, -1)
+template <typename Real, typename Real3>
+GRACE_HOST void plane_parallel_rays(
+    Ray* const d_rays_ptr,
+    const int width,
+    const int height,
+    const Real3 base,
+    const Real3 w,
+    const Real3 h,
+    const Real length)
+{
+    const size_t N_rays = static_cast<size_t>(width) * height;
+
+    Real3 delta_w, delta_h, base_centre, direction;
+
+    delta_w.x = w.x / width;
+    delta_w.y = w.y / width;
+    delta_w.z = w.z / width;
+
+    delta_h.x = h.x / height;
+    delta_h.y = h.y / height;
+    delta_h.z = h.z / height;
+
+    base_centre.x = base.x + (delta_w.x + delta_h.x) / 2.;
+    base_centre.y = base.y + (delta_w.y + delta_h.y) / 2.;
+    base_centre.z = base.z + (delta_w.z + delta_h.z) / 2.;
+
+    direction = normalize3(cross(w, h));
+
+    const int blocks = min(grace::MAX_BLOCKS,
+                           (int) ((N_rays + RAYS_THREADS_PER_BLOCK - 1)
+                                   / RAYS_THREADS_PER_BLOCK));
+    gpu::plane_parallel_rays_kernel<<<blocks, RAYS_THREADS_PER_BLOCK>>>(
+        width,
+        height,
+        N_rays,
+        base_centre,
+        delta_w,
+        delta_h,
+        length,
+        direction,
+        d_rays_ptr);
+    GRACE_KERNEL_CHECK();
+}
+
+
+// d_rays will be resized only if its current size is too small to contain
+// width * height rays; it will never be reduced in size.
+//
+// width and height: the dimensions of the grid of rays to generate
+// base: a point at one corner of the ray-grid plane
+// w: a vector defining the width of the plane, beginning from base
+//    n = width rays are generated along w from base, for m = height rows
+// h: a vector defining the height of the plane, beginning from base
+//    m = height rays are generated along h from base, for n = width columns
+// length: the length of all rays
+//
+// A grid of width * height cells, covering an area |w| * |h|, is produced.
+// The centre of each cell in this grid defines a ray origin. base is located at
+// one corner of this grid. Hence, no ray will have origin == base.
+// The direction of all rays is normalize(cross(w, h)).
+//
+// For rays originating at z = 10, travelling in the -z direction, and covering
+// an area |w| * |h| = 5 * 6 = 30:
+// base = (5, 0, 10)
+// w = (-5, 0, 0)
+// h = (0, 6, 0)
+// direction = normalize(cross(w, h)) = normalize((0, 0, -30)) = (0, 0, -1)
+template <typename Real, typename Real3>
+GRACE_HOST void plane_parallel_rays(
+    thrust::device_vector<Ray>& d_rays,
+    const int width,
+    const int height,
+    const Real3 base,
+    const Real3 w,
+    const Real3 h,
+    const Real length)
+{
+    const size_t N_rays = static_cast<size_t>(width) * height;
+    if (d_rays.size() < N_rays) {
+        d_rays.resize(N_rays);
+    }
+    Ray* const d_rays_ptr = thrust::raw_pointer_cast(d_rays.data());
+
+    plane_parallel_rays(d_rays_ptr, width, height, base, w, h, length);
 }
 
 } // namespace grace
--- tests/helper/rays.cuh
+++ tests/helper/rays.cuh
@@ -3,70 +3,47 @@
 #include "ray.h"
 
 #include "device/morton.cuh"
+#include "kernels/gen_rays.cuh"
 
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
 #include <thrust/sort.h>
 
-// Generates orthogonal rays in the +z direction, covering all points in the
-// range (mins.x/y/z - maxs.w) to (maxs.x/y/z + maxs.w).
+// Generates orthogonal rays in the +z direction, covering all ray-grid cells in
+// the range (mins.x/y/z - maxs.w) to (maxs.x/y/z + maxs.w).
+// The very edge points are not included, because ray origins are the centres
+// of cells in the ray-grid.
 // min/maxs.w may be safely set to zero to avoid edge effects.
 void orthogonal_rays_z(const size_t N_side,
                        const float4 mins, const float4 maxs,
                        thrust::device_vector<grace::Ray>& d_rays,
                        float* area = NULL)
 {
-    const size_t N_rays = N_side * N_side;
-    thrust::host_vector<grace::Ray> h_rays(N_rays);
-    thrust::host_vector<grace::uinteger32> h_keys(N_rays);
-
     // Rays emitted from box side (x, y, mins.z - maxs.w) and of length
     // (maxs.z + maxs.w) - (mins.z - maxs.w).
-    // Since we generally want to fully include all
-    // particles, the ray (ox, oy) limits are set by:
+    // Since we generally want to fully include all particles, the ray (ox, oy)
+    // limits are set by:
     //   [mins.x - maxs.w, maxs.x + maxs.w] and
     //   [mins.y - maxs.w, maxs.y + maxs.w].
     // Rays at the edges are likely to have no hits!
-    float2 spacer;
-    float3 O;
-    float4 span;
 
-    span.x = 2 * maxs.w + maxs.x - mins.x;
-    span.y = 2 * maxs.w + maxs.y - mins.y;
-    span.z = 2 * maxs.w + maxs.z - mins.z;
+    // maxs.w ~ maximum SPH radius. Offset x, y and z on all sides by this value
+    // to avoid clipping particle volumes.
+    const float span_x = maxs.x - mins.x + 2 * maxs.w;
+    const float span_y = maxs.y - mins.y + 2 * maxs.w;
+    const float span_z = maxs.z - mins.z + 2 * maxs.w;
 
-    spacer.x = span.x / (N_side - 1);
-    spacer.y = span.y / (N_side - 1);
+    const float3 base = make_float3(mins.x - maxs.w,
+                                    mins.y - maxs.w,
+                                    mins.z - maxs.w);
+    const float3 w = make_float3(span_x, 0.f, 0.f);
+    const float3 h = make_float3(0.f, span_y, 0.f);
 
-    O.x = mins.x - maxs.w;
-    O.z = mins.z - maxs.w;
+    grace::plane_parallel_rays(d_rays, N_side, N_side, base, w, h, span_z);
 
-    for (int i = 0; i < N_side; ++i, O.x += spacer.x)
-    {
-        O.y = mins.y - maxs.w;
-
-        for (int j = 0; j < N_side; ++j, O.y += spacer.y)
-        {
-            h_rays[i * N_side + j].dx = 0.0f;
-            h_rays[i * N_side + j].dy = 0.0f;
-            h_rays[i * N_side + j].dz = 1.0f;
-
-            h_rays[i * N_side + j].ox = O.x;
-            h_rays[i * N_side + j].oy = O.y;
-            h_rays[i * N_side + j].oz = O.z;
-
-            h_rays[i * N_side + j].length = span.z;
-
-            // Since all rays are PPP, base key on origin instead.
-            // Floats must be in (0, 1) for morton_key().
-            h_keys[i * N_side + j]
-                = grace::morton::morton_key((O.x - mins.x) / span.x,
-                                            (O.y - mins.y) / span.y,
-                                            0.0f);
-        }
+    if (area != NULL) {
+        const float cell_x = span_x / N_side;
+        const float cell_y = span_y / N_side;
+        *area = cell_x * cell_y;
     }
-    thrust::sort_by_key(h_keys.begin(), h_keys.end(), h_rays.begin());
-
-    d_rays = h_rays;
-    if (area != NULL) *area = spacer.x * spacer.y;
 }
