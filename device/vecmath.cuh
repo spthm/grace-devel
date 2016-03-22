676710f2f9b20acbe24d525b2d83786182619d27
Sam Thomson <sp.thomson@outlook.com>
2016-03-22 12:17:05 +0000

    fixup! Added on-GPU generate of plane-parallel rays. Implements #3.

---
+++ device/vecmath.cuh
@@ -0,0 +1,47 @@
+#include <cmath>
+
+namespace grace {
+
+template <typename Real3>
+GRACE_HOST_DEVICE Real3 normalize3(const Real3 v)
+{
+    #ifdef __CUDA_ARCH__
+
+    #if __CUDACC_VER_MAJOR__ >= 7
+    double N = rnorm3d(v.x, v.y, v.z);
+    #else
+    double N = rsqrt(v.x*v.x + v.y*v.y + v.z*v.z);
+    #endif
+
+    #else
+    double N = 1. / std::sqrt(v.x*v.x + v.y*v.y + v.z*v.z);
+    #endif
+
+    Real3 normv;
+    normv.x = v.x * N;
+    normv.y = v.y * N;
+    normv.z = v.z * N;
+
+    return normv;
+}
+
+template <typename Real3>
+GRACE_HOST_DEVICE double dot3(const Real3 u, const Real3 v)
+{
+    return u.x * v.x + u.y * v.y + u.z * v.z;
+}
+
+// NB: only defined for three-component vectors.
+template <typename Real3>
+GRACE_HOST_DEVICE Real3 cross(const Real3 u, const Real3 v)
+{
+    Real3 result;
+
+    result.x = u.y * v.z - u.z * v.y;
+    result.y = u.z * v.x - u.x * v.z;
+    result.z = u.x * v.y - u.y * v.x;
+
+    return result;
+}
+
+} // namespace grace
