530ca927d2b0ed2fb2b61862e4f166564e6db1e8
Sam Thomson <sp.thomson@outlook.com>
2016-05-24 12:24:03 +0100

    Added scan API functions to cuda/scan.cuh.

--- include/grace/cuda/kernels/scan.cuh
+++ include/grace/cuda/kernels/scan.cuh
@@ -3,85 +3,59 @@
 #include "grace/error.h"
 #include "grace/types.h"
 
-#include "grace/external/sgpu/kernels/segscancsr.cuh"
-
 #include <thrust/device_vector.h>
 
 namespace grace {
 
-namespace gpu {
+namespace detail {
 
-template <typename Real>
-__global__ void multiply_by_weights(const Real* unweighted,
-                                    const Real* weights,
-                                    const unsigned int* weight_map,
-                                    size_t N_unweighted,
-                                    Real* weighted)
+template <typename Real, typename IdxType>
+__global__ void multiply_by_weights_kernel(
+    const Real* unweighted,
+    const Real* weights,
+    const IdxType* weight_map,
+    size_t N_unweighted,
+    Real* weighted)
 {
-    unsigned int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    unsigned int weight_index;
-
-    while (tid < N_unweighted)
+    for(unsigned int tid = threadIdx.x + blockIdx.x * blockDim.x;
+        tid < N_unweighted;
+        tid += blockDim.x * gridDim.x)
     {
-        weight_index = weight_map[tid];
+        IdxType weight_index = weight_map[tid];
         weighted[tid] = weights[weight_index] * unweighted[tid];
-        tid += blockDim.x * gridDim.x;
     }
 }
 
-} // namespace gpu
-
-// d_data and d_results may be the same vector.
-template <typename Real>
-GRACE_HOST void exclusive_segmented_scan(
-    const thrust::device_vector<int>& d_segment_offsets,
-    thrust::device_vector<Real>& d_data,
-    thrust::device_vector<Real>& d_results)
+template <typename Real, typename IdxType>
+GRACE_HOST void multiply_by_weights(
+    const Real* unweighted,
+    const Real* weights,
+    const IdxType* weight_map,
+    const size_t N_unweighted,
+    Real* weighted)
 {
-    // SGPU calls require a context.
-    int device_ID = 0;
-    GRACE_CUDA_CHECK(cudaGetDevice(&device_ID));
-    sgpu::ContextPtr sgpu_context_ptr = sgpu::CreateCudaDevice(device_ID);
-
-    std::auto_ptr<sgpu::SegCsrPreprocessData> pp_data_ptr;
-
-    size_t N_data = d_data.size();
-    size_t N_segments = d_segment_offsets.size();
-
-    sgpu::SegScanCsrPreprocess<Real>(
-        N_data, thrust::raw_pointer_cast(d_segment_offsets.data()),
-        N_segments, true, &pp_data_ptr, *sgpu_context_ptr);
-
-    sgpu::SegScanApply<sgpu::SgpuScanTypeExc>(*pp_data_ptr,
-        thrust::raw_pointer_cast(d_data.data()), Real(0), sgpu::plus<Real>(),
-        thrust::raw_pointer_cast(d_results.data()), *sgpu_context_ptr);
+    detail::multiply_by_weights_kernel<<<48, 512>>>(
+        unweighted, weights, weight_map, N_unweighted, weighted
+    );
+    GRACE_KERNEL_CHECK();
 }
 
-// d_weight_map should behave such that the weight for a value at index i in
-// d_to_sum is located at index j = d_weight_map[i] in d_weights.
-// That is,
-//     weighted_values[i] = d_to_sum[i] * d_weights[d_weight_map[i]]
-template <typename Real>
-GRACE_HOST void weighted_exclusive_segmented_scan(
-    const thrust::device_vector<Real>& d_to_sum,
+template <typename Real, typename IdxType>
+GRACE_HOST void multiply_by_weights(
+    const thrust::device_vector<Real>& d_unweighted,
     const thrust::device_vector<Real>& d_weights,
-    const thrust::device_vector<unsigned int>& d_weight_map,
-    const thrust::device_vector<int>& d_segment_offsets,
-    thrust::device_vector<Real>& d_sum)
+    const thrust::device_vector<IdxType>& d_weight_map,
+    thrust::device_vector<Real>& d_weighted)
 {
-    // Initialize the weighted inputs, d_weighted.
-    thrust::device_vector<Real> d_weighted(d_to_sum.size());
-    gpu::multiply_by_weights<<<48, 512>>>(
-        thrust::raw_pointer_cast(d_to_sum.data()),
+    multiply_by_weights(
+        thrust::raw_pointer_cast(d_unweighted.data()),
+        d_unweighted.size(),
         thrust::raw_pointer_cast(d_weights.data()),
         thrust::raw_pointer_cast(d_weight_map.data()),
-        d_weighted.size(),
-        thrust::raw_pointer_cast(d_weighted.data()));
-    GRACE_KERNEL_CHECK();
-
-    // Segmented, cumulative sum such that d_sum[i] = cumulative sum *up to*
-    // (not including, i.e. an exclusive sum) the ith weighted value.
-    grace::exclusive_segmented_scan(d_segment_offsets, d_weighted, d_sum);
+        thrust::raw_pointer_cast(d_weighted.data())
+    );
 }
+
+} // namespace detail
 
 } // namespace grace
---
+++ include/grace/cuda/scan.cuh
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "grace/types.h"
+
+#include "grace/external/sgpu/kernels/segscancsr.cuh"
+
+#include <thrust/device_vector.h>
+
+namespace grace {
+
+// d_data and d_results may be the same vector.
+template <typename Real>
+GRACE_HOST void exclusive_segmented_scan(
+    const thrust::device_vector<int>& d_segment_offsets,
+    thrust::device_vector<Real>& d_data,
+    thrust::device_vector<Real>& d_results)
+{
+    // SGPU calls require a context.
+    int device_ID = 0;
+    GRACE_CUDA_CHECK(cudaGetDevice(&device_ID));
+    sgpu::ContextPtr sgpu_context_ptr = sgpu::CreateCudaDevice(device_ID);
+
+    std::auto_ptr<sgpu::SegCsrPreprocessData> pp_data_ptr;
+
+    size_t N_data = d_data.size();
+    size_t N_segments = d_segment_offsets.size();
+
+    sgpu::SegScanCsrPreprocess<Real>(
+        N_data, thrust::raw_pointer_cast(d_segment_offsets.data()),
+        N_segments, true, &pp_data_ptr, *sgpu_context_ptr);
+
+    sgpu::SegScanApply<sgpu::SgpuScanTypeExc>(*pp_data_ptr,
+        thrust::raw_pointer_cast(d_data.data()), Real(0), sgpu::plus<Real>(),
+        thrust::raw_pointer_cast(d_results.data()), *sgpu_context_ptr);
+}
+
+// d_weight_map should behave such that the weight for a value at index i in
+// d_to_sum is located at index j = d_weight_map[i] in d_weights.
+// That is,
+//     weighted_values[i] = d_to_sum[i] * d_weights[d_weight_map[i]]
+template <typename Real>
+GRACE_HOST void weighted_exclusive_segmented_scan(
+    const thrust::device_vector<Real>& d_to_sum,
+    const thrust::device_vector<Real>& d_weights,
+    const thrust::device_vector<unsigned int>& d_weight_map,
+    const thrust::device_vector<int>& d_segment_offsets,
+    thrust::device_vector<Real>& d_sum)
+{
+    // Initialize the weighted inputs, d_weighted.
+    thrust::device_vector<Real> d_weighted(d_to_sum.size());
+    detail::multiply_by_weights(d_to_sum, d_weights, d_weight_map, d_weighted);
+
+    // Segmented, cumulative sum such that d_sum[i] = cumulative sum *up to*
+    // (not including, i.e. an exclusive sum) the ith weighted value.
+    grace::exclusive_segmented_scan(d_segment_offsets, d_weighted, d_sum);
+}
+
+} // namespace grace
