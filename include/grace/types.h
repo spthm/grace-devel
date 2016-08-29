c55f645c3b758d1a8141b1ec888e0984d08b2ed0
Sam Thomson <sp.thomson@outlook.com>
2016-08-30 00:09:09 +0100

    Add fixed-width integer typdefs for more systems.

--- include/grace/types.h
+++ include/grace/types.h
@@ -79,10 +79,23 @@
 
 namespace grace {
 
+#if defined(__GNUC__) || defined(__clang__) || defined(__APPLE__) && defined(__MACH__)
 typedef uint32_t uinteger32;
 typedef uint64_t uinteger64;
 typedef int32_t integer32;
 typedef int64_t integer64;
+#elif define(_MSC_VER)
+typedef unsigned __int32 uinteger32;
+typedef unsigned __int64 uinteger64;
+typedef __int32 integer32;
+typedef __int64 integer64;
+#else
+#warning Compiler or system not detected, key bit-lengths may be incorrect
+typedef unsigned int uinteger32;
+typedef unsigned long long uinteger64;
+typedef int integer32;
+typedef long long integer64;
+#endif
 
 // Binary encoding with +ve = 1, -ve = 0.
 // Octants for ray generation.
