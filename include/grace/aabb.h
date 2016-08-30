07e12dcc548eb915f3faa1f2a7345f582b75994e
S Thomson <spth@roe.ac.uk>
2016-08-30 16:26:03 +0100

    Add AABB struct.

---
+++ include/grace/aabb.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "grace/types.h"
+#include "grace/vector.h"
+
+namespace grace {
+
+template <typename T>
+GRACE_ALIGNED_STRUCT(16) AABB
+{
+    typedef T value_type;
+    typedef Vector<3, T> vec_type;
+
+    Vector<3, T> min;
+    Vector<3, T> max;
+
+    // Unit box at origin.
+    GRACE_HOST_DEVICE AABB();
+
+    // U must be convertible to T.
+    template <typename U>
+    GRACE_HOST_DEVICE AABB(const Vector<3, U>& min, const Vector<3, U>& max);
+
+    // U must be convertible to T.
+    template <typename U>
+    GRACE_HOST_DEVICE AABB(const U min[3], const U max[3]);
+
+    // U must be convertible to T.
+    template <typename U>
+    GRACE_HOST_DEVICE AABB(const AABB<U>& other);
+
+#ifdef __CUDACC__
+    // Unit box at origin.
+    // float must be convertible to T.
+    GRACE_HOST_DEVICE AABB(const float3& min, const float3& max);
+
+    // Unit box at origin.
+    // double must be convertible to T.
+    GRACE_HOST_DEVICE AABB(const double3& min, const double3& max);
+#endif
+
+    GRACE_HOST_DEVICE T area();
+
+    GRACE_HOST_DEVICE Vector<3, T> center();
+
+    GRACE_HOST_DEVICE Vector<3, T> size();
+
+    GRACE_HOST_DEVICE void scale(const T s);
+    GRACE_HOST_DEVICE void scale(const Vector<3, T>& vec);
+
+    GRACE_HOST_DEVICE void translate(const Vector<3, T>& vec);
+};
+
+
+//
+// Comparison operations
+//
+
+template <typename T>
+GRACE_HOST_DEVICE
+bool operator==(const AABB<T>& lhs, const AABB<T>& rhs);
+
+template <typename T>
+GRACE_HOST_DEVICE
+bool operator!=(const AABB<T>& lhs, const AABB<T>& rhs);
+
+} // namespace grace
+
+#include "grace/detail/aabb-inl.h"
---
+++ include/grace/detail/aabb-inl.h
@@ -0,0 +1,109 @@
+#pragma once
+
+#include "grace/aabb.h"
+
+namespace grace {
+
+//
+// Constructors and member functions
+//
+
+template <typename T>
+GRACE_HOST_DEVICE AABB<T>::AABB()
+    : min((T)-0.5, (T)-0.5, (T)-0.5), max((T)0.5, (T)0.5, (T)0.5) {}
+
+template <typename T>
+template <typename U>
+GRACE_HOST_DEVICE AABB<T>::AABB(const Vector<3, U>& min, const Vector<3, U>& max)
+    : min(min), max(max) {}
+
+template <typename T>
+template <typename U>
+GRACE_HOST_DEVICE AABB<T>::AABB(const U min[3], const U max[3])
+    : min(min), max(max) {}
+
+template <typename T>
+template <typename U>
+GRACE_HOST_DEVICE AABB<T>::AABB(const AABB<U>& other)
+    : min(other.min), max(other.max) {}
+
+#ifdef __CUDACC__
+template <typename T>
+GRACE_HOST_DEVICE AABB<T>::AABB(const float3& min, const float3& max)
+    : min(min), max(max) {}
+
+template <typename T>
+GRACE_HOST_DEVICE AABB<T>::AABB(const double3& min, const double3& max)
+    : min(min), max(max) {}
+#endif
+
+template <typename T>
+GRACE_HOST_DEVICE T AABB<T>::area()
+{
+    const Vector<3, T> s = size();
+    return T(2) * (s.x * s.y + s.x * s.z + s.y * s.z);
+}
+
+template <typename T>
+GRACE_HOST_DEVICE Vector<3, T> AABB<T>::center()
+{
+    return T(0.5) * (max + min);
+}
+
+template <typename T>
+GRACE_HOST_DEVICE Vector<3, T> AABB<T>::size()
+{
+    return max - min;
+}
+
+template <typename T>
+GRACE_HOST_DEVICE void AABB<T>::scale(const T s)
+{
+    const Vector<3, T> c = center();
+
+    min = min - c; max = max - c;
+
+    min = min * s; max = max * s;
+
+    min = min + c; max = max + c;
+}
+
+template <typename T>
+GRACE_HOST_DEVICE void AABB<T>::scale(const Vector<3, T>& vec)
+{
+    const Vector<3, T> c = center();
+
+    min = min - c; max = max - c;
+
+    min = min * vec; max = max * vec;
+
+    min = min + c; max = max + c;
+}
+
+template <typename T>
+GRACE_HOST_DEVICE void AABB<T>::translate(const Vector<3, T>& vec)
+{
+    min = min + vec;
+    max = max + vec;
+}
+
+
+//
+// Comparison operations
+//
+
+template <typename T>
+GRACE_HOST_DEVICE
+bool operator==(const AABB<T>& lhs, const AABB<T>& rhs)
+{
+    return lhs.min == rhs.min && lhs.max == rhs.max;
+}
+
+template <typename T>
+GRACE_HOST_DEVICE
+bool operator!=(const AABB<T>& lhs, const AABB<T>& rhs)
+{
+    return lhs.min != rhs.min || lhs.max != rhs.max;
+}
+
+} // namespace grace
