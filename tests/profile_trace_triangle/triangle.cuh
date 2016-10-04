fed7bb1c21096876c8e0314eab5c06eff6f3227e
S Thomson <spth@roe.ac.uk>
2016-10-04 19:06:13 +0100

    Fix compile-time errors. Mostly typos, missing __device__ delcarations, missing includes.

--- tests/profile_trace_triangle/triangle.cuh
+++ tests/profile_trace_triangle/triangle.cuh
@@ -1,6 +1,9 @@
 #pragma once
 
-#include "helper/vector_math.h"
+#include "helper/vector_math.cuh"
+
+#include "grace/cuda/util/bound_iter.cuh"
+#include "grace/cuda/nodes.h"
 
 #define TRIANGLE_EPSILON 0.000001
 
@@ -10,14 +13,20 @@ struct Triangle
     float3 e1;
     float3 e2;
 
-    GRACE_HOST_DEVICE Triangle(float3 vertex, float3 edge1, float3 edge2)
+    // Must be constructable on the device.
+    // thrust::sort[_by_key] requires a default constructor.
+    __host__ __device__ Triangle() {}
+    __host__ __device__ Triangle(float3 vertex, float3 edge1, float3 edge2)
         : v(vertex), e1(edge1), e2(edge2) {}
 };
 
 struct RayData_tri
 {
+    // We can make use of some of GRACE's built-in generic traversal functors
+    // if our RayData type has a public .data member.
+    int data;
     float t_min;
-}
+};
 
 struct RayIntersect_tri
 {
@@ -42,9 +51,10 @@ struct RayIntersect_tri
      * The implementation here is that presented by Moeller and, Trumbore, with
      * an additional check on t at the end.
      */
-    GRACE_DEVICE bool operator()(
+    // grace::gpu::BoundIter is not callable on the host.
+    __device__ bool operator()(
         const grace::Ray& ray, const Triangle& tri,
-        const RayData_tri& ray_data, const int /*lane*/,
+        RayData_tri& ray_data, const int /*lane*/,
         const grace::gpu::BoundIter<char> /*sm_iter*/) const
     {
         float3 dir = make_float3(ray.dx, ray.dy, ray.dz);
@@ -71,51 +81,62 @@ struct RayIntersect_tri
         float t = dot_product(tri.e2, Q) * inv_det;
         // If true, the intersection is too far along the ray, or before the ray
         // origin.
-        if (t > ray_data.tmin || t > ray.length || t < TRIANGLE_EPSILON) return false;
+        if (t > ray_data.t_min || t > ray.length || t < TRIANGLE_EPSILON) return false;
 
         ray_data.t_min = t;
         return true;
     }
 };
 
+struct OnHit_tri
+{
+    // grace::gpu::BoundIter is not callable on the host.
+    __device__ void operator()(const int /*ray_idx*/, const grace::Ray&,
+                                 RayData_tri& ray_data, const int tri_idx,
+                                 const Triangle&, const int /*lane*/,
+                                 const grace::gpu::BoundIter<char> /*smem_iter*/) const
+    {
+        ray_data.data = tri_idx;
+    }
+};
+
 struct RayEntry_tri
 {
-    GRACE_DEVICE void operator()(const int /*ray_idx*/, const grace::Ray& ray,
-                                 const RayData_tri& ray_data,
-                                 const grace::gpu::BoundIter<char> /*smem_iter*/)
+    // grace::gpu::BoundIter is not callable on the host.
+    __device__ void operator()(const int /*ray_idx*/, const grace::Ray& ray,
+                                 RayData_tri& ray_data,
+                                 const grace::gpu::BoundIter<char> /*smem_iter*/) const
     {
-        ray_data.tmin = ray.length;
+        ray_data.data = -1;
+        ray_data.t_min = ray.length + TRIANGLE_EPSILON;
     }
-}
-
-struct RayExit_tri
-{
-    GRACE_DEVICE void operator()(const int /*ray_idx*/, const grace::Ray&,
-                                 const RayData_tru&,
-                                 const grace::gpu::BoundIter<char> /*smem_iter*/)
-}
+};
 
 struct TriangleAABB
 {
+    // Must be callable from the device.
+    __host__ __device__
     void operator()(const Triangle& tri, float3* bot, float3* top) const
     {
         float3 v0 = tri.v;
         float3 v1 = v0 + tri.e1;
         float3 v2 = v0 + tri.e2;
 
-        *bot.x = min(v0.x, min(v1.x, v2.x));
-        *bot.y = min(v0.y, min(v1.y, v2.y));
-        *bot.z = min(v0.z, min(v1.z, v2.z));
+        bot->x = min(v0.x, min(v1.x, v2.x));
+        bot->y = min(v0.y, min(v1.y, v2.y));
+        bot->z = min(v0.z, min(v1.z, v2.z));
 
-        *top.x = max(v0.x, max(v1.x, v2.x));
-        *top.y = max(v0.y, max(v1.y, v2.y));
-        *top.z = max(v0.z, max(v1.z, v2.z));
+        top->x = max(v0.x, max(v1.x, v2.x));
+        top->y = max(v0.y, max(v1.y, v2.y));
+        top->z = max(v0.z, max(v1.z, v2.z));
     }
 };
 
 struct TriangleCentroid
 {
-    float3 operator()(const Triangle& tri)
+    // Must be callable from the device.
+    __host__ __device__
+    float3 operator()(const Triangle& tri) const
     {
         // For triangle with vertices V0, V1 and V2, the centroid is located at
         // (1/3) * (V0 + V1 + V2).
